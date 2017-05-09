529f62408f4fc322b33bcbeac0100166bfc54256
Sean Enck <enckse@gmail.com>
2017-05-09 09:33:08 -0400

    cleaning up as it starts to work for '.'

--- .vim/plugin/vim_completions.py
+++ .vim/plugin/vim_completions.py
@@ -11,12 +11,14 @@ ADDED_ATTRS = ["__" + x + "__" for x in ["dict",
                                          "mro",
                                          "subclasses"]]
 
+
 def get_selections(segment, inputs, reversing):
+    """Get selections given input."""
     before = []
     matched = None
     after = []
     print(segment)
-    for ins in inputs:
+    for ins in sorted(set(inputs)):
         if matched is not None:
             after.append(ins)
         if segment.startswith(ins):
@@ -24,16 +26,15 @@ def get_selections(segment, inputs, reversing):
         if not matched:
             before.append(ins)
     if reversing:
-        result = after
-    else:
         result = list(reversed(before))
+    else:
+        result = after
     if matched is None:
         matched = ""
     return (result, matched)
 
+
 try:
-    built = dir(__builtins__)
-    print(built)
     pos = vim.current.window.cursor
     if pos and pos[0] > 0 and pos[1] > 0:
         row = pos[0] - 1
@@ -41,10 +42,11 @@ try:
         cur = vim.current.buffer[row]
         ch = cur[col]
         reversing = vim.eval("a:direction") == "1"
-        # test.__init__(test test)
         col_off = col + 1
+        replacing = None
+        after_attr = None
         if ch == ".":
-            dot_attr = set(dir(object) + ADDED_ATTRS)
+            dot_attr = dir(object) + ADDED_ATTRS
             after = cur[col_off:]
             results = get_selections(after, dot_attr, reversing)
             remaining = results[0]
@@ -53,7 +55,10 @@ try:
             else:
                 replacing = remaining[0]
             after_attr = results[1]
-            new_cur = cur[0:col_off] + replacing + cur[col_off + len(after_attr):]
+        if replacing is not None and after_attr is not None:
+            new_cur = "{}{}{}".format(cur[0:col_off],
+                                      replacing,
+                                      cur[col_off + len(after_attr):])
             vim.current.buffer[row] = new_cur
 except Exception as e:
     print(str(e))
