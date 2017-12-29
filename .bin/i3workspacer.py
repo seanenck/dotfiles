45457ec159630f581316ed7f29c84b9e23e4555f
Sean Enck <enckse@gmail.com>
2017-12-29 12:13:02 -0500

    rewritten - no ipc :/

--- .bin/i3workspacer.py
+++ .bin/i3workspacer.py
@@ -3,13 +3,14 @@
 handles processing and naming i3 workspaces based on content
 """
 
-import i3ipc
 import subprocess 
 import os
 import signal
+import time
+import json
+import hashlib
 from subprocess import check_output
 
-conn = i3ipc.Connection()
 window_cache = {}
 devnull = open(os.devnull, 'w')
 
@@ -34,41 +35,74 @@ def get_pid(name):
 def uncurl(obj):
     """uncurl nest nodes down."""
     children = []
-    for node in obj.nodes:
+    for node in obj["nodes"]:
         for opened in uncurl(node):
             children.append(opened)
         children.append(node)
     return children
 
 
-def on_change(self, event):
+
+def on_change(hashed, debug):
     """on interested changes, call this."""
     try:
-        tree = conn.get_tree()
-        for t in tree.workspaces():
-            rename_workspaces(t)
+        tree = check_output(["i3-msg", "-t", "get_tree"])
+        m = hashlib.md5()
+        m.update(tree)
+        cur = m.digest()
+        if cur == hashed:
+            if debug:
+                print("no change")
+            return cur
+        j = json.loads(tree)
+        workspaces = _get_workspaces(j)
+        _rename_workspaces(workspaces)
         _is_active()
-    except:
-        pass
+        return cur
+    except Exception as e:
+        if debug:
+            print(e)
+        return hashed
 
+def _get_workspaces(j):
+    """Get workspaces."""
+    matched = {}
+    for node in uncurl(j):
+        if "num" in node and "name" in node:
+            n = node["num"]
+            name = node["name"]
+            if n and n >= 1:
+                children = []
+                for c in uncurl(node):
+                    if "window_properties" in c:
+                        props = c["window_properties"]
+                        if "class" in props:
+                            classed = props["class"]
+                            children.append(classed)
+                matched[n] = (name, children)
+    return matched
 
-def rename_workspaces(e):
+def _rename_workspaces(workspaces):
     """rename a workspace."""
-    all_nodes = []
-    for node in uncurl(e):
-        if node is None or node.window_class is None:
+    for k in workspaces:
+        vals = workspaces[k]
+        if len(vals[1]) == 0:
             continue
-        all_nodes.append(node)
-    named = str(e.num)
-    if len(all_nodes) > 0:
-        names = [x.window_class[0:20] for x in all_nodes]
-        named = named + ":" + ",".join(names)
-    named = named.lower()
-    if e.num in window_cache and window_cache[e.num] == named:
-        return
-    window_cache[e.num] = named
-    subprocess.call(["i3-msg", "rename workspace \"{}\" to \"{}\"".format(e.name, named)], stdout=devnull, stderr=subprocess.STDOUT)
+        named = ",".join([x[0:20].lower() for x in vals[1]])
+        named = str(k) + ":" + named
+        subprocess.call(["i3-msg",
+                         "rename workspace \"{}\" to \"{}\"".format(vals[0], named)],
+                         stdout=devnull,
+                         stderr=subprocess.STDOUT)
 
-conn.on('workspace', on_change)
-conn.on('window', on_change)
-conn.main()
+
+def main():
+    """Main entry."""
+    last_hash = None
+    while True:
+        last_hash = on_change(last_hash, False)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
