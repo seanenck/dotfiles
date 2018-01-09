d13f3e8fb3103724845e095f3d4e10b2244ab23f
Sean Enck <enckse@gmail.com>
2018-01-09 07:18:35 -0500

    using hooks

--- .bin/secondly.sh
+++ .bin/secondly.sh
@@ -11,15 +11,6 @@ if [ ! -e $HAS_RAN ]; then
     done
     touch $HAS_RAN
 fi
-
-if [ -e $GIT_CHANGES ]; then
-    last=$(stat $GIT_CHANGES | grep ^Modify | cut -d " " -f 2,3 | cut -d "." -f 1)
-    last_d=$(date -d "$last" +%s)
-    cur_d=$(date -d "5 minutes ago" +%s)
-    if [ $last_d -lt $cur_d ]; then
-        rm -f $GIT_CHANGES
-    fi
-fi
 if [ ! -e $GIT_CHANGES ]; then
     git-changes > $GIT_CHANGES
 fi
