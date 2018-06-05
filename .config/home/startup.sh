c99f3205230e251e276444f93a349760dd9bf5e3
Sean Enck <enckse@gmail.com>
2018-06-05 14:41:50 -0400

    move this all to one place

--- .config/home/startup.sh
+++ .config/home/startup.sh
@@ -6,6 +6,9 @@ if [ ! -e '/var/cache/.start' ]; then
     echo '=====NOTICE====='
     echo ''
     sudo locale-gen
+    sudo chmod 644 /etc/environment
     sudo touch '/var/cache/.start'
 fi
+DISPLAY=:0
+export DISPLAY
 source /home/enck/.bash_aliases
