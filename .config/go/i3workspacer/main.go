08e1f87e58fd73759513d7eaf2d6e7cc681ca710
Sean Enck <enckse@gmail.com>
2018-08-07 16:33:02 -0400

    working on conversion some more

--- .config/go/i3workspacer/main.go
+++ .config/go/i3workspacer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/mdirkse/i3ipc"
+	"strings"
 )
 func main() {
 	ipcsocket, err := i3ipc.GetIPCSocket()
@@ -15,14 +16,37 @@ func main() {
 		panic("unable to subscribe")
 	}
 	for {
-	    event := <-ws_events
+	    <-ws_events
 		tree, err := ipcsocket.GetTree()
 		if err != nil {
 			continue
 		}
 		workspaces := tree.Workspaces()
 		for _, w := range workspaces {
+			names := uncurl(w.Nodes)
+			text := ""
+			if len(names) > 0 {
+				text = strings.Join(names, ",")
+			}
+			ids := strings.Split(w.Name, ":")
+			fmt.Printf("%s:%s\n", ids[0], text)
 		}
-	    fmt.Printf("Received an event: %v\n", event)
 	}
+}
+
+func uncurl(nodes []i3ipc.I3Node) []string {
+	var result []string
+	if len (nodes) > 0 {
+		for _, n := range nodes {
+			name := n.Window_Properties.Class
+			if len(name) > 0 {
+				named := strings.Split(strings.ToLower(name), " ")
+				result = append(result, named[0])
+			}
+			for _, r := range uncurl(n.Nodes) {
+				result = append(result, r)
+			}
+		}
+	}
+	return result
 }
