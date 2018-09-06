789fa7307b559da58f7b7f0771ec12d53597ea5d
Sean Enck <enckse@gmail.com>
2018-09-05 20:51:10 -0400

    generate locales

--- .config/home/startup.sh
+++ .config/home/startup.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-if [ ! -e '/var/cache/.start' ]; then 
-    echo ''
-    echo '=====NOTICE====='
-    echo 'performing first-time startup'
-    echo '=====NOTICE====='
-    echo ''
-    sudo locale-gen
-    sudo chmod 644 /etc/environment
-    sudo touch '/var/cache/.start'
-fi
 DISPLAY=:0
 export DISPLAY
 source /home/enck/.bash_aliases
