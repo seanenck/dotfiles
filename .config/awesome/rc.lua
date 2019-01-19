6caadd156486aefcabc297b2a09cd5369d3e929b
Sean Enck <enckse@voidedtech.com>
2019-01-19 13:32:48 -0500

    move widgets out

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -6,211 +6,12 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 require("awful.hotkeys_popup.keys")
+local widgets = require("widgets")
 
-local use_font = "DejaVu Sans 12"
+local use_font = widgets.font
 
 local sysclock = wibox.widget.textclock("    %a %Y-%m-%d %X ", 1)
 sysclock.font = use_font
-
-local USER_HOME = "/home/enck/"
-local HOME_BIN = USER_HOME .. ".local/bin/"
-local STATS = HOME_BIN .. "status "
-local USER_TMP = USER_HOME .. ".cache/home/tmp/"
-local SYS_ONLINE = USER_TMP .. "sys.online"
-local STAT_RESET = USER_TMP .. "stats.reset"
-
-local function format_output(output)
-    return "    " .. output
-end
-
-local function call(script)
-    local f = io.popen(script, 'r')
-    local s = f:read('*a')
-    f:close()
-    return s:match("^%s*(.-)%s*$")
-end
-
-local function file_exists(name)
-   local f=io.open(name,"r")
-   if f ~= nil then
-       io.close(f)
-       return true
-   else
-       return false
-   end
-end
-
-local function locking()
-    local res = call(HOME_BIN .. "locking status")
-    if res == '' or res == nil then
-        return ""
-    else
-        return format_output(res)
-    end
-end
-
-local function online()
-    local avail = file_exists(SYS_ONLINE)
-    call(STATS .. "online")
-end
-
-local function ipv4(prefix, iface, online)
-    local addr = call("ip addr | grep " .. iface .. " | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/' | grep '[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*$' | sed 's/^/" .. prefix .. ": /g'")
-    if addr == '' or addr == nil then
-        return nil
-    else
-        return addr
-    end
-end
-
-local function network()
-    local avail = online()
-    local wired = ipv4("E", "enp0s31f6", avail)
-    if wired == nil then
-        wired = ipv4("E", "enx0050b6abfe1b", avail)
-    end
-    local wireless = ipv4("W", "wlp3s0", avail)
-    if wired == nil and wireless == nil then
-        naughty.notify({ title = "WARNING", text = "OFFLINE", bg = "#ff0000", timeout = 10, position = "bottom_right" })
-        return format_output("OFFLINE")
-    else
-        local out = ""
-        if wired ~= nil then
-            out = out .. format_output(wired)
-        end
-        if wireless ~= nil then
-            out = out .. format_output(wireless)
-        end
-        return out
-    end
-end
-
-local function stats()
-    local results = ""
-    for k, v in pairs({"git", "email"}) do
-        local stat = call(STATS .. v)
-        if stat ~= '' and stat ~= nil then
-            results = results .. format_output(stat)
-        end
-    end
-    return results
-end
-
-local function brightness()
-    local res = tonumber(call('xrandr --current --verbose | grep "Brightness" | cut -d ":" -f 2 | sed "s/0\\.//g" | sed "s/1\\.0/100/g" | tail -n 1 | awk \'{printf "%3.0f", $1}\' | sed "s/^[ \\t]*//g"'))
-    local val = string.format("%3d", res)
-    return format_output("* " .. val .. "%")
-end
-
-local function battery()
-    local battery = 0
-    local power = "("
-    local drain = tonumber(call("cat /sys/class/power_supply/AC/online")) == 0
-    for k, v in pairs({"0", "1"}) do
-        local perc = tonumber(call("cat /sys/class/power_supply/BAT" .. v .. "/capacity"))
-        battery = battery + perc
-        power = power .. string.format("%3d", perc)
-        if k == 1 then
-            power = power .. ","
-        end
-    end
-    power = power .. ")%"
-    local bind = "+"
-    if drain then
-        bind = "-"
-        if battery < 20 then
-            naughty.notify({ title = "WARNING", text = "BATTERY LOW", bg = "#ff0000", timeout = 30, position = "bottom_left" })
-        end
-    end
-    return format_output("[" .. bind .. "]" .. power)
-end
-
-local lock_widget = wibox.widget{
-    markup = locking(),
-    align  = 'center',
-    valign = 'center',
-    font = use_font,
-    widget = wibox.widget.textbox
-}
-
-local battery_widget = wibox.widget{
-    markup = battery(),
-    align  = 'center',
-    valign = 'center',
-    font = use_font,
-    widget = wibox.widget.textbox
-}
-
-local function volume_cmd(cmd)
-    return call(HOME_BIN .. "volume " .. cmd)
-end
-
-local function volume()
-    local muted = volume_cmd("ismute") == "true"
-    local sound = ""
-    if muted then
-        sound = "|"
-    else
-        sound = "="
-    end
-    sound = "&lt;" .. sound .. string.format(" %3d%%", tonumber(volume_cmd("volume")))
-    return format_output(sound)
-end
-
-local net_widget = wibox.widget{
-    markup = network(),
-    align  = 'center',
-    valign = 'center',
-    font = use_font,
-    widget = wibox.widget.textbox
-}
-
-local stats_widget = wibox.widget{
-    markup = stats(),
-    align  = 'center',
-    valign = 'center',
-    font = use_font,
-    widget = wibox.widget.textbox
-}
-
-local brightness_widget = wibox.widget{
-    markup = brightness(),
-    align  = 'center',
-    valign = 'center',
-    font = use_font,
-    widget = wibox.widget.textbox
-}
-
-local volume_widget = wibox.widget{
-    markup = volume(),
-    align  = 'center',
-    valign = 'center',
-    font = use_font,
-    widget = wibox.widget.textbox
-}
-
-local function setup_timers()
-    local t = timer({timeout = 1})
-    local cnt = 0
-    t:connect_signal("timeout", function()
-        if file_exists(STAT_RESET) then
-            cnt = 11
-            call("rm -f " .. STAT_RESET)
-        end
-        if cnt > 10 then
-            battery_widget:set_markup(battery())
-            net_widget:set_markup(network())
-            volume_widget:set_markup(volume())
-            brightness_widget:set_markup(brightness())
-            stats_widget:set_markup(stats())
-            lock_widget:set_markup(locking())
-            cnt = 0
-        else
-            cnt = cnt + 1
-        end
-    end)
-    t:start()
-end
 
 -- {{{ Error handling
 if awesome.startup_errors then
@@ -298,12 +99,12 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            lock_widget,
-            stats_widget,
-            battery_widget,
-            brightness_widget,
-            volume_widget,
-            net_widget,
+            widgets.lock_widget,
+            widgets.stats_widget,
+            widgets.battery_widget,
+            widgets.brightness_widget,
+            widgets.volume_widget,
+            widgets.net_widget,
             sysclock,
             wibox.widget.systray(),
             s.mylayoutbox,
@@ -531,4 +332,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 awful.spawn.with_shell("subsystem workspaces 1")
 awful.spawn.with_shell("xautolock -time 5 -locker '/home/enck/.local/bin/locking lock'")
 awful.spawn.with_shell("status")
-setup_timers()
+widgets.setup_timers()
---
+++ .config/awesome/widgets.lua
@@ -0,0 +1,209 @@
+local gears = require("gears")
+local wibox = require("wibox")
+local naughty = require("naughty")
+
+local widgets = {}
+use_font = "DejaVu Sans 12"
+widgets.font = use_font
+
+local USER_HOME = "/home/enck/"
+local HOME_BIN = USER_HOME .. ".local/bin/"
+local STATS = HOME_BIN .. "status "
+local USER_TMP = USER_HOME .. ".cache/home/tmp/"
+local SYS_ONLINE = USER_TMP .. "sys.online"
+local STAT_RESET = USER_TMP .. "stats.reset"
+
+local function format_output(output)
+    return "    " .. output
+end
+
+local function call(script)
+    local f = io.popen(script, 'r')
+    local s = f:read('*a')
+    f:close()
+    return s:match("^%s*(.-)%s*$")
+end
+
+local function file_exists(name)
+   local f=io.open(name,"r")
+   if f ~= nil then
+       io.close(f)
+       return true
+   else
+       return false
+   end
+end
+
+local function locking()
+    local res = call(HOME_BIN .. "locking status")
+    if res == '' or res == nil then
+        return ""
+    else
+        return format_output(res)
+    end
+end
+
+local function online()
+    local avail = file_exists(SYS_ONLINE)
+    call(STATS .. "online")
+end
+
+local function ipv4(prefix, iface, online)
+    local addr = call("ip addr | grep " .. iface .. " | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/' | grep '[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*$' | sed 's/^/" .. prefix .. ": /g'")
+    if addr == '' or addr == nil then
+        return nil
+    else
+        return addr
+    end
+end
+
+local function network()
+    local avail = online()
+    local wired = ipv4("E", "enp0s31f6", avail)
+    if wired == nil then
+        wired = ipv4("E", "enx0050b6abfe1b", avail)
+    end
+    local wireless = ipv4("W", "wlp3s0", avail)
+    if wired == nil and wireless == nil then
+        naughty.notify({ title = "WARNING", text = "OFFLINE", bg = "#ff0000", timeout = 10, position = "bottom_right" })
+        return format_output("OFFLINE")
+    else
+        local out = ""
+        if wired ~= nil then
+            out = out .. format_output(wired)
+        end
+        if wireless ~= nil then
+            out = out .. format_output(wireless)
+        end
+        return out
+    end
+end
+
+local function stats()
+    local results = ""
+    for k, v in pairs({"git", "email"}) do
+        local stat = call(STATS .. v)
+        if stat ~= '' and stat ~= nil then
+            results = results .. format_output(stat)
+        end
+    end
+    return results
+end
+
+local function brightness()
+    local res = tonumber(call('xrandr --current --verbose | grep "Brightness" | cut -d ":" -f 2 | sed "s/0\\.//g" | sed "s/1\\.0/100/g" | tail -n 1 | awk \'{printf "%3.0f", $1}\' | sed "s/^[ \\t]*//g"'))
+    local val = string.format("%3d", res)
+    return format_output("* " .. val .. "%")
+end
+
+local function battery()
+    local battery = 0
+    local power = "("
+    local drain = tonumber(call("cat /sys/class/power_supply/AC/online")) == 0
+    for k, v in pairs({"0", "1"}) do
+        local perc = tonumber(call("cat /sys/class/power_supply/BAT" .. v .. "/capacity"))
+        battery = battery + perc
+        power = power .. string.format("%3d", perc)
+        if k == 1 then
+            power = power .. ","
+        end
+    end
+    power = power .. ")%"
+    local bind = "+"
+    if drain then
+        bind = "-"
+        if battery < 20 then
+            naughty.notify({ title = "WARNING", text = "BATTERY LOW", bg = "#ff0000", timeout = 30, position = "bottom_left" })
+        end
+    end
+    return format_output("[" .. bind .. "]" .. power)
+end
+
+local function volume_cmd(cmd)
+    return call(HOME_BIN .. "volume " .. cmd)
+end
+
+local function volume()
+    local muted = volume_cmd("ismute") == "true"
+    local sound = ""
+    if muted then
+        sound = "|"
+    else
+        sound = "="
+    end
+    sound = "&lt;" .. sound .. string.format(" %3d%%", tonumber(volume_cmd("volume")))
+    return format_output(sound)
+end
+
+widgets.lock_widget = wibox.widget{
+    markup = locking(),
+    align  = 'center',
+    valign = 'center',
+    font = use_font,
+    widget = wibox.widget.textbox
+}
+
+widgets.battery_widget = wibox.widget{
+    markup = battery(),
+    align  = 'center',
+    valign = 'center',
+    font = use_font,
+    widget = wibox.widget.textbox
+}
+
+widgets.net_widget = wibox.widget{
+    markup = network(),
+    align  = 'center',
+    valign = 'center',
+    font = use_font,
+    widget = wibox.widget.textbox
+}
+
+widgets.stats_widget = wibox.widget{
+    markup = stats(),
+    align  = 'center',
+    valign = 'center',
+    font = use_font,
+    widget = wibox.widget.textbox
+}
+
+widgets.brightness_widget = wibox.widget{
+    markup = brightness(),
+    align  = 'center',
+    valign = 'center',
+    font = use_font,
+    widget = wibox.widget.textbox
+}
+
+widgets.volume_widget = wibox.widget{
+    markup = volume(),
+    align  = 'center',
+    valign = 'center',
+    font = use_font,
+    widget = wibox.widget.textbox
+}
+
+function widgets.setup_timers()
+    local t = timer({timeout = 1})
+    local cnt = 0
+    t:connect_signal("timeout", function()
+        if file_exists(STAT_RESET) then
+            cnt = 11
+            call("rm -f " .. STAT_RESET)
+        end
+        if cnt > 10 then
+            widgets.battery_widget:set_markup(battery())
+            widgets.net_widget:set_markup(network())
+            widgets.volume_widget:set_markup(volume())
+            widgets.brightness_widget:set_markup(brightness())
+            widgets.stats_widget:set_markup(stats())
+            widgets.lock_widget:set_markup(locking())
+            cnt = 0
+        else
+            cnt = cnt + 1
+        end
+    end)
+    t:start()
+end
+
+return widgets
