29b150199e800f7ddb4e29370634d88f9bbe4edd
Sean Enck <enckse@voidedtech.com>
2019-03-09 20:48:20 -0500

    moving over enterkeys

--- .local/bin/common.py
+++ .local/bin/common.py
@@ -4,6 +4,17 @@ import os
 from subprocess import Popen, PIPE
 
 
+def get_output_or_error(command):
+    """Get output or error from command."""
+    p = Popen(command, stdout=PIPE)
+    output, err = p.communicate()
+    if err:
+        return (None, err)
+    if p.returncode != 0:
+        return (None, Exception("unable to read stdout"))
+    return (output, None)
+
+
 class Object(object):
     """Environment object."""
 
@@ -19,16 +30,11 @@ def read_env(file_name="home/common"):
     """Read the environment for my user."""
     home_env = os.environ["HOME"]
     home = os.path.join(home_env, ".config", file_name)
-    p = Popen(["bash",
-               "-c",
-               "source " + home + "; _exports"],
-              stdout=PIPE)
-    output, err = p.communicate()
-    if err:
+    output, err = get_output_or_error(["bash",
+                                       "-c", 
+                                       "source " + home + "; _exports"])
+    if err is not None:
         raise err
-    rc = p.returncode
-    if rc != 0:
-        raise Exception("unable to read exports")
     lines = [x for x in output.decode("utf-8").split("\n") if x]
     result = Object()
     result.HOME = home_env
