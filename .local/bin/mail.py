6bf66269251ab4407f6f02e4b566b3b70f896f91
Sean Enck <enckse@voidedtech.com>
2019-03-13 21:30:02 -0400

    moving mail counts to be a function call

--- .local/bin/mail.py
+++ .local/bin/mail.py
@@ -6,6 +6,41 @@ import argparse
 import os
 from email.mime.message import MIMEMessage
 import email.utils
+import common
+
+ACCOUNTS = [common.GMAIL_ACCOUNT, common.FMAIL_ACCOUNT]
+
+
+def _countdirs(dirs, account, ind):
+    """Count dir emails."""
+    cnt = 0
+    for d in dirs:
+        cnt += len(os.listdir(d))
+    if cnt > 0:
+        return "{} {}({})".format(cnt, account, ind)
+
+
+def count(env):
+    """Get unread counts."""
+    results = []
+    for a in ACCOUNTS:
+        unread = []
+        new = []
+        acct = os.path.join(env.MAIL_DIR, a)
+        for root, dirs, _ in os.walk(acct):
+            for d in dirs:
+                rootd = os.path.join(root, d)
+                if "Trash" in rootd:
+                    continue
+                if d == "cur":
+                    if "Filtered" in rootd or "Spam" in rootd:
+                        if "Filtered/Automated" not in rootd:
+                            unread.append(rootd)
+                elif d == "new":
+                    new.append(rootd)
+        results.append(_countdirs(new, a, "n"))
+        results.append(_countdirs(unread, a, "u"))
+    return [x for x in results if x]
 
 
 def main():
