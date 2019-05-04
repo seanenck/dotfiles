01569bec8133238d3b7d44158a6b3c30f04f9606
Sean Enck <enckse@voidedtech.com>
2019-05-04 18:13:26 -0400

    subdir added

--- .local/bin/common.py
+++ .local/bin/common.py
@@ -72,7 +72,7 @@ def touch(file_name):
 
 def _env_adj(env):
     """Environment adjustment."""
-    env.REPOSITORY = os.path.join(env.PERM_LOCATION, "repository") + "/"
+    env.REPOSITORY = os.path.join(env.PERM_LOCATION, "managed", "repository") + "/"
     env.BASE_SERVER = "fact"
     return env
 
