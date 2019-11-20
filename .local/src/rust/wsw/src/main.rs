460fc044fab9adc7732732199a04280e01cb7f36
Sean Enck <enckse@voidedtech.com>
2019-11-20 07:39:22 -0500

    set hostname

--- .local/src/rust/wsw/src/main.rs
+++ .local/src/rust/wsw/src/main.rs
@@ -32,6 +32,7 @@ struct Context {
     bind: String,
     url: String,
     online: String,
+    host: String,
 }
 
 impl Context {
@@ -48,18 +49,20 @@ impl Context {
         }
         None
     }
-    fn to_server(&self) -> (String, String, String) {
+    fn to_server(&self) -> (String, String, String, String) {
         (
             self.configs.to_owned(),
             self.cache.to_owned(),
             self.bind.to_owned(),
+            self.host.to_owned(),
         )
     }
-    fn from_server(configs: String, cache: String, bind: String) -> Context {
+    fn from_server(configs: String, cache: String, bind: String, host: String) -> Context {
         Context {
             configs,
             cache,
             bind,
+            host,
             url: "".to_string(),
             online: "".to_string(),
         }
@@ -179,7 +182,7 @@ fn set_link(link_name: String, up: bool) {
     }
 }
 
-fn update(networks: String, cache: String, profile: Profile) {
+fn update(networks: String, cache: String, hostname: String, profile: Profile) {
     let interfaces = list_interfaces(false);
     let cmd = Command::new("dhclient").arg("-r").output();
     match cmd {
@@ -235,6 +238,16 @@ fn update(networks: String, cache: String, profile: Profile) {
             }
         }
     });
+    match Command::new("hostnamectl")
+        .arg("set-hostname")
+        .arg(hostname)
+        .output()
+    {
+        Ok(_) => {}
+        Err(e) => {
+            println!("hostnamectl failed: {}", e);
+        }
+    }
     println!("change completed: {}", &profile);
 }
 
@@ -264,7 +277,12 @@ fn change_network(ctx: &Context, profile: String) {
     let p = to_profile(ctx, profile.to_string());
     match p {
         Some(obj) => {
-            update(ctx.configs.to_string(), ctx.cache.to_string(), obj);
+            update(
+                ctx.configs.to_string(),
+                ctx.cache.to_string(),
+                ctx.host.to_string(),
+                obj,
+            );
         }
         None => {
             println!("invalid profile for change: {}", &profile);
@@ -272,8 +290,8 @@ fn change_network(ctx: &Context, profile: String) {
     }
 }
 
-fn run_monitor(config: String, cache: String, bind: String) {
-    let ctx = Context::from_server(config, cache, bind);
+fn run_monitor(config: String, cache: String, bind: String, host: String) {
+    let ctx = Context::from_server(config, cache, bind, host);
     if let Some(p) = ctx.current_profile() {
         change_network(&ctx, format!("{}", p));
     }
@@ -315,7 +333,7 @@ fn daemonize(ctx: &Context) {
     });
     let server = ctx.to_server();
     let monitor = thread::spawn(move || {
-        run_monitor(server.0, server.1, server.2);
+        run_monitor(server.0, server.1, server.2, server.3);
     });
     monitor.join().expect("monitor thread unavailable");
     online.join().expect("unable to wait for online thread");
@@ -513,7 +531,17 @@ fn main() {
         .value_of("cache")
         .unwrap_or("/var/cache/wsw")
         .to_string();
+    let mut hostname = String::new();
+    match fs::read_to_string("/etc/hostname") {
+        Ok(contents) => {
+            hostname.push_str(contents.trim());
+        }
+        Err(e) => {
+            panic!("unable to read hostname: {}", e);
+        }
+    };
     let ctx = Context {
+        host: hostname.to_string(),
         bind: matches
             .value_of("bind")
             .unwrap_or("127.0.0.1:6789")
