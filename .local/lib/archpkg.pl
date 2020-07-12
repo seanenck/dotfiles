a4bebc44922705081f68c22d44f71935fbe52f65
Sean Enck <enckse@voidedtech.com>
2020-07-12 12:34:40 -0400

    move this

--- .local/lib/archpkg.pl
+++ .local/lib/archpkg.pl
@@ -8,5 +8,10 @@ for ( ( "log", "tar.zst" ) ) {
     system("rm -f *.$_");
 }
 
+my $makepkg = "/tmp/makepkg.conf";
+system("cat /etc/makepkg.conf \$HOME/.makepkg.conf > $makepkg");
+system("sudo install -Dm644 $makepkg \$CHROOT/root/etc/makepkg.conf");
+unlink $makepkg;
+
 die "packaging failed"
   if system("makechrootpkg -c -n -d /var/cache/pacman/pkg -r \$CHROOT") != 0;
