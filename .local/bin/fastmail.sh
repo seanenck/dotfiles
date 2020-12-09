f44810bb1abc2c78970847b7d7be4303bdcb69e2
Sean Enck <enckse@voidedtech.com>
2020-12-09 18:15:03 -0500

    change how these are started

--- .local/bin/fastmail.sh
+++ .local/bin/fastmail.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/kitty --title=fastmail -d=/home/enck/downloads /usr/bin/mutt -F /home/enck/.mutt/fastmail.muttrc
+/usr/bin/kitty --start-as=maximized --title=fastmail -d=/home/enck/downloads /usr/bin/mutt -F /home/enck/.mutt/fastmail.muttrc
