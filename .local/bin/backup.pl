a3196891e1e6cc1d35f9bdf076013ba43cc67cf3
Sean Enck <enckse@voidedtech.com>
2021-01-18 10:55:45 -0500

    deprecated

--- .local/bin/backup.pl
+++ .local/bin/backup.pl
@@ -11,21 +11,10 @@ $check = $tmp . $check;
 exit 0 if -e $check;
 
 my $server  = $ENV{"SERVER"};
-my $mirrors = $ENV{"HOME"} . "/store/mirror/";
 
 if ( system("curl -Is http://$server > /dev/null") != 0 ) {
     print "backup system not available\n";
     exit 0;
-}
-
-if ( -d $mirrors ) {
-    print "backing up mirrors\n";
-    for (`ls $mirrors`) {
-        chomp;
-        system(
-            "rsync -avcr --delete-after $mirrors/$_/ rsync://$server/mirror/$_/"
-        );
-    }
 }
 
 system("rsync -av /var/cache/voidedtech/backup/ rsync://$server/backup/");
