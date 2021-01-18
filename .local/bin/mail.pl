bc1f74e9de08e536ddaf607b042b4a3d8ea337bb
Sean Enck <enckse@voidedtech.com>
2021-01-18 18:44:36 -0500

    mail script

---
+++ .local/bin/mail.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $home         = $ENV{"HOME"};
+my $local    = "$home/.local/bin/";
+my $script          = "perl ${local}mail.pl";
+my $sys          = "${local}sys";
+my $imap  = "$home/store/imap";
+
+if (@ARGV) {
+    my $command = $ARGV[0];
+    if ( $command eq "poll" ) { 
+        exit if ( system("$sys online") != 0 );
+        my $mail_dir = "/tmp/mail";
+        mkdir $mail_dir if !-d $mail_dir;
+        my $time = `date +%Y-%m-%d-%H-`;
+        chomp $time;
+        $time .= `date +%M` >= 30 ? "30" : "00";
+        my $mail_file = $mail_dir . "/$time";
+        my $mutt      = $mail_dir . "/mutt";
+
+        if ( system("pgrep -x mutt > /dev/null") == 0 ) {
+            system("touch $mutt");
+            exit;
+        }
+        if ( -e $mutt ) {
+            system("rm -f $mutt $mail_file");
+        }
+
+        exit if -e "$mail_file";
+        system("$script sync");
+        system("touch $mail_file");
+    }
+    elsif ( $command eq "sync" ) {
+        if ( -d $imap ) {
+            exit 0 if system("pgrep -x mbsync > /dev/null") == 0;
+            my $mbsync = "/tmp/mbsync";
+            mkdir $mbsync if ( !-d $mbsync );
+            system("notify-send -t 5000 'syncing mail'");
+            for ( ( "mbsync -a", "notmuch new" ) ) {
+                system("$_ | systemd-cat -t 'mbsync'");
+            }
+        }
+    }
+    else {
+        die "unknown command";
+    }
+}
+
+while (1) {
+    system("$script poll");
+    sleep 15;
+}
