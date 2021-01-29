3c16daef3658c8102fef28bbf888b9ba41dfa475
Sean Enck <enckse@voidedtech.com>
2021-01-29 10:15:22 -0500

    working on container wrapper

---
+++ .local/containers/youtube-dl.Dockerfile
@@ -0,0 +1,5 @@
+FROM docker.io/alpine:latest
+
+RUN apk --no-cache add youtube-dl
+RUN mkdir /build
+WORKDIR /build
---
+++ .local/lib/containers.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $dir = $ENV{"HOME"} . "/.local/containers/";
+
+my $containers = `find $dir -type f -name "*.Dockerfile" -exec basename {} \\; | sed 's/\.Dockerfile//g'`;
+
+if (!@ARGV) {
+    print "clean $containers";
+    exit 0;
+}
+
+my $cmd = shift @ARGV;
+
+if ( $cmd eq "clean" ) {
+    system("podman ps -a -q | xargs podman rm");
+    exit 0;
+}
+
+my $file = "${dir}$cmd.Dockerfile";
+die "unknown container: $cmd" if !-e $file;
+
+my $tag = "$cmd";
+my $run = "";
+if ( $cmd eq "youtube-dl" ) {
+    $run = "--volume=/home/enck/downloads:/build $tag youtube-dl";
+    my $target = shift @ARGV;
+    die "no target URL given" if !$target;
+    $run = "$run '$target'";
+}
+
+die "unknown command: $cmd" if !$run;
+
+die "unable to build" if system("podman build --tag $tag -f $file") != 0;
+
+system("podman run $run");
