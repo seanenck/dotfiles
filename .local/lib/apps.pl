5eeeede3b19247d9267215f6467e1dbace6db09f
Sean Enck <enckse@voidedtech.com>
2021-01-30 08:27:42 -0500

    getting local apps to work again

--- .local/lib/apps.pl
+++ .local/lib/apps.pl
@@ -14,4 +14,26 @@ if ( $cmd ne "wofi" ) {
     print "unknown command\n";
     exit 0;
 }
-system("echo $apps | tr ' ' '\\n' | sed 's/firefox/firefox-developer-edition/g' | wofi --show dmenu -i --style=\$HOME/.config/wofi/style.css | \${SHELL:-'/bin/sh'}");
+
+my %local;
+for (`find $ENV{"HOME"}/.local/apps -type l`) {
+    chomp;
+    if ( $_ ) {
+        my $name = `basename $_`;
+        chomp $name;
+        $local{"$name"} = $_;
+        $apps = "$apps $name";
+    }
+}
+my $chosen = `echo $apps | tr ' ' '\\n' | sed 's/firefox/firefox-developer-edition/g' | wofi --show dmenu -i --style=\$HOME/.config/wofi/style.css`;
+chomp $chosen;
+if ( !$chosen ) {
+    exit 0;
+}
+
+if ( exists($local{$chosen}) ) {
+    system("bash $local{$chosen}");
+    exit 0;
+}
+
+system("$chosen");
