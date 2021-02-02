d33fcded8ac60f3fafcd41b7bd88778a9a4c24f4
Sean Enck <enckse@voidedtech.com>
2021-02-01 20:05:40 -0500

    common problems

--- .local/lib/sway.py
+++ .local/lib/sway.py
@@ -21,14 +21,12 @@ def main():
         _set_master(i3, args.master_scale_width, args.master_scale_height, args.screen_offset_width, args.screen_offset_height, True)
     elif args.mode == "move-focus":
         _move_focus(i3)
-    elif args.mode == "reset":
-        _reset(i3)
+    elif args.mode in ["reset", "reset-all"]:
+        _reset(i3, "all" in args.mode)
     elif args.mode == "workspace-focus":
         _workspace_focus(i3)
-    elif args.mode == "resize-right":
-        _resize(i3, True, args.screen_offset_width, args.screen_offset_height, args.resize_rate)
-    elif args.mode == "resize-left":
-        _resize(i3, False, args.screen_offset_width, args.screen_offset_height, args.resize_rate)
+    elif args.mode in ["resize-right", "resize-left"]:
+        _resize(i3, "right" in args.mode, args.screen_offset_width, args.screen_offset_height, args.resize_rate)
     elif args.mode in ["move-left", "move-right", "move-up", "move-down"]:
         _move(i3, args.mode.replace("move-", ""))
 
@@ -52,13 +50,20 @@ def _positional_compare(mode):
         return _right    
 
 
+def _is_floating(target):
+    return target.type == "floating_con"
+
+
+def _get_unfocused(focused):
+    return [x for x in focused.workspace() if x != focused and x.name]
+
+
 def _move(i3, mode):
     focused = i3.get_tree().find_focused()
-    is_floating = focused.type == "floating_con"
-    if is_floating:
+    if _is_floating(focused):
         _command(focused, "move {}".format(mode))
         return
-    windows = [x for x in focused.workspace() if x != focused and x.name]
+    windows = _get_unfocused(focused)
     if len(windows) == 0:
         return
     fxn = _positional_compare(mode)
@@ -79,7 +84,7 @@ def _move(i3, mode):
 
 def _workspace_focus(i3):
     focused = i3.get_tree().find_focused()
-    windows = [x for x in focused.workspace() if x != focused]
+    windows = _get_unfocused(focused)
     if len(windows) == 0:
         return
     sort = {}
@@ -97,10 +102,13 @@ def _workspace_focus(i3):
         lowest = sort[obj[0]]
     _command(lowest, "focus")
 
+
+def _get_active_output(i3):
+    return [x for x in i3.get_outputs() if x.focused][0]
+
 def _resize(i3, right, offset_width, offset_height, resize_rate):
     focused = i3.get_tree().find_focused()
-    is_floating = focused.type == "floating_con"
-    if not is_floating:
+    if not _is_floating(focused):
         cmd = "shrink"
         if right:
             cmd = "grow"
@@ -110,18 +118,20 @@ def _resize(i3, right, offset_width, offset_height, resize_rate):
     if not right:
         delta = -1 * delta
     w = focused.rect.width + delta
-    active = [x for x in i3.get_outputs() if x.focused][0]
+    active = _get_active_output(i3)
     w_offset = active.rect.x + offset_width
     h_offset = active.rect.y + offset_height
     _command(focused, "resize set width {}".format(w))
     _command(focused, "move absolute position {} {}".format(w_offset, h_offset))
 
 
-def _reset(i3):
+def _reset(i3, force):
     focused = i3.get_tree().find_focused()
     windows = [x for x in focused.workspace() if x != focused]
     for w in [focused] + windows:
         _command(w, "floating disable")
+        if force and not w.name:
+            _command(w, "kill")
 
 
 def _move_focus(i3):
@@ -132,11 +142,10 @@ def _move_focus(i3):
 
 def _fullscreen(i3, to_width, to_height, offset_width, offset_height):
     focused = i3.get_tree().find_focused()
-    is_floating = focused.type == "floating_con"
-    if is_floating:
+    if _is_floating(focused):
         _command(focused, "floating disable")
         return
-    active = [x for x in i3.get_outputs() if x.focused][0]
+    active = _get_active_output(i3)
     w = int(active.rect.width * to_width)
     h = int(active.rect.height * to_height)
     _command(focused, "floating enable")
@@ -154,10 +163,10 @@ def _command(obj, command):
 
 def _set_master(i3, to_width, to_height, offset_width, offset_height, root_call):
     focused = i3.get_tree().find_focused()
-    windows = [x for x in focused.workspace() if x != focused]
+    windows = _get_unfocused(focused)
     if len(windows) == 0:
         return
-    active = [x for x in i3.get_outputs() if x.focused][0]
+    active = _get_active_output(i3)
     if active.rect.width < active.rect.height:
         return
     if root_call:
