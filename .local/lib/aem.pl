b94f64e29ea8e385133ad11e2df9355ddaf1b650
Sean Enck <enckse@voidedtech.com>
2021-02-05 20:59:01 -0500

    this is part of drudge

--- .local/lib/aem.pl
+++ .local/lib/aem.pl
@@ -15,8 +15,6 @@ my $server     = $ENV{"REMOTE_SERVER"};
 my $ssh        = "ssh  $server -- ";
 my $build_root = "$build/root";
 my $home       = $ENV{"HOME"};
-my $aem_base   = "$home/.local/var/aem/";
-my $flag_base  = "${aem_base}flagged";
 my $gpg_key    = "031E9E4B09CFD8D3F0ED35025109CDF607B5BB04";
 my $self       = "perl $home/.local/lib/aem.pl";
 
@@ -163,74 +161,8 @@ elsif ( $command eq "schroot" ) {
     system("schroot -c chroot:dev");
     exit 0;
 }
-elsif ( $command eq "flagged" ) {
-    system("mkdir -p $flag_base") if !-d $flag_base;
-    my $redir    = "2>&1 | systemd-cat -t aem-flagged";
-    my $tmp_flag = `mktemp`;
-    chomp $tmp_flag;
-    system("date +%Y-%m-%dT%H:%M:%S $redir");
-    my %remotes;
-    my %filters;
-    $remotes{"baseskel"}    = "git://cgit.voidedtech.com/skel";
-    $remotes{"devskel"}     = "git://cgit.voidedtech.com/skel";
-    $remotes{"serverskel"}  = "git://cgit.voidedtech.com/skel";
-    $remotes{"corescripts"} = "git://cgit.voidedtech.com/corescripts";
-    $remotes{"sysmon"}      = "git://cgit.voidedtech.com/sysmon";
-    $remotes{"voidedtech"}  = "git://cgit.voidedtech.com/whoami";
-    $remotes{"pass-git"}    = "https://git.zx2c4.com/password-store";
-    $filters{"voidedtech"}  = "src/";
-
-    for my $base ( ( "base", "dev", "server" ) ) {
-        $filters{"${base}skel"} = "$base/";
-    }
-
-    my @notices;
-
-    my %done;
-    for my $package (`pacman -Sl vpr | cut -d " " -f 2`) {
-        chomp $package;
-        next if !$package;
-        if ( !exists $remotes{$package} ) {
-            next;
-        }
-        system("echo $package $redir");
-        my $remote      = $remotes{$package};
-        my @remote_obj  = split( "/", $remote );
-        my $remote_base = "$flag_base/$remote_obj[-1]";
-        if ( !-d $remote_base ) {
-            die "unable to clone"
-              if system("git clone --depth=1 $remote $remote_base $redir") != 0;
-        }
-        if ( !exists( $done{$remote_base} ) ) {
-            for my $cmd ( ( "fetch", "pull" ) ) {
-                die "git command $cmd failed for $package"
-                  if system("git -C $remote_base $cmd $redir") != 0;
-            }
-            $done{$remote_base} = $remote_base;
-        }
-        my $filter = ".";
-        if ( exists( $filters{$package} ) ) {
-            $filter = $filters{$package};
-        }
-        my $date =
-`git -C $remote_base log -1 --format=%cd --date=format:%Y%m%d.%H%M%S $filter`;
-        chomp $date;
-        my $vers =
-`pacman -Ss $package | grep 'vpr/$package' | cut -d " " -f 2 | rev | cut -d "-" -f 2- | rev`;
-        chomp $vers;
-        die "unable to read package version for: $package" if !$vers;
-        my $compare = $vers cmp $date;
-        if ( $compare < 0 ) {
-            push @notices, "out-of-date:$package";
-        }
-    }
-    if (@notices) {
-        my $notify = join( "\n", @notices );
-        print $notify, "\n";
-    }
-}
 elsif ( $command eq "help" ) {
-    print "run sync makepkg repo-add schroot pacstrap flagged detach-sign";
+    print "run sync makepkg repo-add schroot pacstrap detach-sign";
 }
 else {
     die "unknown command $command";
