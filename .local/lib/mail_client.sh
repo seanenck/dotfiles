32dbc0871434a7540f6b5fa3f645e7c465bc823a
Sean Enck <enckse@voidedtech.com>
2021-02-06 13:05:29 -0500

    don't curl

--- .local/lib/mail_client.sh
+++ .local/lib/mail_client.sh
@@ -16,7 +16,7 @@ MAILHOST=$LOCAL_SERVER
 if [ ! -z "$1" ]; then
     case $1 in
         "new")
-            curl -s http://$MAILHOST/files/messages/new.txt | sed 's/^/mail:/g'
+            drudge ask $MAILHOST mail | sed 's/^/mail:/g'
             ;;
     esac
     exit 0
