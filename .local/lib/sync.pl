0b88437fd555149ec97dab740361e5e9db711077
Sean Enck <enckse@voidedtech.com>
2021-02-07 09:02:27 -0500

    fixing backups for now

--- .local/lib/sync.pl
+++ .local/lib/sync.pl
@@ -110,7 +110,7 @@ my $backups    = "$host/backup/";
 my $has_backup = "${sync}backup." . `date +%Y%m%d%p`;
 chomp $has_backup;
 if ( !-e $has_backup ) {
-    if ( system("rsync -av /var/cache/voidedtech/backup/ $backups") == 0 ) {
+    if ( system("rsync -av /var/cache/drudge/backup/ $backups") == 0 ) {
         system("touch $has_backup");
     }
     else {
