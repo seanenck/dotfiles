aa60710b5206c974979c706d68c52827df3aa0d4
Sean Enck <enckse@voidedtech.com>
2021-02-13 08:23:24 -0500

    allow for some timeouts

--- .local/lib/sync.pl
+++ .local/lib/sync.pl
@@ -134,7 +134,7 @@ for my $other (@others) {
         }
     }
     else {
-        system("notify-send 'sync: lastsync failed ($other)'");
+        system("notify-send -t 10000 'sync: lastsync failed ($other)'");
         $do = 0;
     }
     if ( $do == 1 ) {
@@ -145,7 +145,7 @@ for my $other (@others) {
             system("cp $other_curr $other_last");
         }
         else {
-            system("notify-send 'sync: pull failed ($other)'");
+            system("notify-send -t 10000 'sync: pull failed ($other)'");
         }
     }
 }
