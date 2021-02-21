6fdec197b237a9188edb4fd27648bd2f5b434bb3
Sean Enck <enckse@voidedtech.com>
2021-02-20 23:51:41 -0500

    cleaning up

--- .env/thin/lib/autoclip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-source $HOME/.local/env/vars && pass -c autoclip > /dev/null
--- .env/thin/lib/sway.sh
+++ .env/thin/lib/sway.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-source ~/.local/env/vars
 CMD=$1
 if [ -z "$CMD" ]; then
     exit
 fi
+IS_LAPTOP=0
+IS_DESKTOP=0
+if [ -d /sys/class/power_supply/BAT0 ]; then
+    IS_LAPTOP=1
+else
+    IS_DESKTOP=1
+fi
 case $CMD in
     "sleep")
-        if [ -e $IS_LAPTOP ]; then
+        if [ $IS_LAPTOP -eq 1 ]; then
             systemctl suspend
         fi
         ;;
     "outputs")
-        if [ -e $IS_LAPTOP ]; then
+        if [ $IS_LAPTOP -eq 1 ]; then
             swaymsg output eDP-1 pos 0 0 res 3840x2160
         fi
-        if [ -e $IS_DESKTOP ]; then
+        if [ $IS_DESKTOP -eq 1 ]; then
             swaymsg output DP-2 transform 90
         fi
         ;;
