256476f0d5f0dab286747101cb2045bd063f2bee
Sean Enck <enckse@voidedtech.com>
2021-05-07 17:56:08 -0400

    need iconv

---
+++ .config/voidedtech/devtools.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [ 
+        pkgs.buildPackages.golangci-lint
+        pkgs.buildPackages.go_1_16
+        pkgs.buildPackages.cargo
+        pkgs.buildPackages.clippy
+        pkgs.buildPackages.libiconv
+    ];
+}
