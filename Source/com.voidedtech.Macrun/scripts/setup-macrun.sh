1db713c61860669135ccc0c946da0a508c2f60b5
Sean Enck <enckse@voidedtech.com>
2021-07-18 14:23:47 -0400

    get vim plugins

--- Source/com.voidedtech.Macrun/scripts/setup-macrun.sh
+++ Source/com.voidedtech.Macrun/scripts/setup-macrun.sh
@@ -2,15 +2,17 @@
 _ready() {
     setup-timezone -z US/Michigan
     setup-ntp -c chrony
+    setup-apkcache /var/cache/apk
     ln -sf $1 /root/store
 }
 
 _setupdisks() {
-    local has store prof files
+    local has store prof files vim
     store=/var/opt/store
+    vim=$store/.vim
     prof=$store/.bash
     files=".bashrc .vimrc .bash_aliases"
-    mkdir -p $prof
+    mkdir -p $prof $vim
     has=0
     blkid | grep -q "vdb1"
     if [ $? -eq 0 ]; then
@@ -27,6 +29,13 @@ _setupdisks() {
         for f in $(echo $files); do
             ln -sf $prof/$f /root/$f
         done
+        vimplug=/root/.vim/pack/dist/
+        for f in $(ls $vim/); do
+            echo "updating $f"
+            git -C $vim/$f pull
+        done
+        mkdir -p $vimplug
+        ln -sf $vim/ ${vimplug}start
         _ready $store
         return
     fi
@@ -34,6 +43,8 @@ _setupdisks() {
     for f in $(echo $files); do
         cp /root/$f $prof/$f
     done
+    git clone https://github.com/vim-airline/vim-airline $vim/vim-airline
+    git clone https://github.com/dense-analysis/ale $vim/ale
     _ready $store
 }
 
