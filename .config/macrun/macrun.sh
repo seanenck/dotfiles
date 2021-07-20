7beaafb2b7ee9037613e9d4755487b21f1ee14d6
Sean Enck <enckse@voidedtech.com>
2021-07-19 21:07:04 -0400

    migration completed

---
+++ .config/macrun/macrun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "#!/bin/bash"
+echo "cp -r root/.vim /root/.vim"
+echo "chown root:root -R /root/.vim/"
+
+mkdir -p root/.vim/
+cp -r $HOME/.vim/pack root/.vim/pack
+
+for f in .bashrc .bash_aliases .vimrc .bash_profile; do
+    cp $HOME/$f root/$f
+    echo "install -Dm644 --owner=root --group=root root/$f /root/$f"
+done
+
