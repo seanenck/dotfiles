3d3c2b3384832aeffb46a97553b5a17c69b56b60
Sean Enck <enckse@voidedtech.com>
2021-07-22 17:26:51 -0400

    this no longer exists

--- .config/macrun/macrun.sh
+++ .config/macrun/macrun.sh
@@ -10,8 +10,3 @@ for f in .bashrc .bash_aliases .vimrc .bash_profile; do
     cp $HOME/$f root/$f
     echo "install -Dm644 --owner=root --group=root root/$f /root/$f"
 done
-
-ping -c1 -W1 can.voidedtech.com >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-    echo 'sed -i "s/dl-cdn.alpinelinux.org/192.168.1.15:9999/g" /etc/apk/repositories'
-fi
