ebe5bf0667ced538d3b8e1efb039e7a6cf89b7b3
Sean Enck <enckse@voidedtech.com>
2021-08-09 15:31:18 -0400

    copy less into the macrun container

--- .config/macrun/configure.sh
+++ .config/macrun/configure.sh
@@ -3,8 +3,8 @@
 echo "cp -r root/.vim /root/.vim"
 echo "chown root:root -R /root/.vim/"
 
-mkdir -p root/.vim/
-cp -r $HOME/.vim/pack root/.vim/pack
+mkdir -p root/.vim/pack/dist/start/
+cp -r $HOME/.vim/pack/dist/start/vim-airline root/.vim/pack/dist/start/
 
 for f in .bashrc .bash_aliases .vimrc .bash_profile; do
     cp $HOME/$f root/$f
