69e551510dcc59ca3f7d9d1731ccb66449ec444a
Sean Enck <enckse@voidedtech.com>
2021-08-10 10:30:55 -0400

    fzf in containers

--- .config/macrun/configure.sh
+++ .config/macrun/configure.sh
@@ -3,13 +3,15 @@
 echo "cp -r root/.vim /root/.vim"
 echo "chown root:root -R /root/.vim/"
 
-mkdir -p root/.vim/pack/dist/start/
-cp -r $HOME/.vim/pack/dist/start/vim-airline root/.vim/pack/dist/start/
+mkdir -p root/.vim/
+cp -r $HOME/.vim/pack/ root/.vim/
 
 for f in .bashrc .bash_aliases .vimrc .bash_profile; do
     cp $HOME/$f root/$f
     echo "install -Dm644 --owner=root --group=root root/$f /root/$f"
 done
+echo "sed -i \"s/system('uname')/'Darwin'/g\" /root/.vimrc"
+echo "sed -i \"s#/opt/local/share/fzf/vim#/usr/share/vim/vimfiles/plugin/fzf.vim#g\" /root/.vimrc"
 
 ping -c1 -W1 can.voidedtech.com >/dev/null 2>&1
 if [ $? -eq 0 ]; then
