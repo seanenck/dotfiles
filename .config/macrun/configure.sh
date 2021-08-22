58f10920bc091bbf8d2e6953af1f8191196037d0
Sean Enck <enckse@voidedtech.com>
2021-08-22 15:24:34 -0400

    rebuilt

--- .config/macrun/configure.sh
+++ .config/macrun/configure.sh
@@ -13,8 +13,8 @@ done
 echo "sed -i \"s/system('uname')/'Darwin'/g\" /root/.vimrc"
 echo "sed -i \"s#/opt/local/share/fzf/vim#/usr/share/vim/vimfiles/plugin/fzf.vim#g\" /root/.vimrc"
 
-curl -s http://can.voidedtech.com > /dev/null 2>&1
+curl -s http://primary.voidedtech.com > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo 'sed -i "s/dl-cdn.alpinelinux.org/192.168.1.15:9999/g" /etc/apk/repositories'
+    echo 'sed -i "s/dl-cdn.alpinelinux.org/192.168.1.10:9999/g" /etc/apk/repositories'
 fi
 echo "apk update"
