cf44832a3cb0c70e5396169be4cc5234765172bc
Sean Enck <enckse@voidedtech.com>
2021-10-13 18:11:57 -0400

    don't setup disk if setup already

--- lib/vmr/lib.init-alpine.sh
+++ lib/vmr/lib.init-alpine.sh
@@ -4,7 +4,12 @@ echo "http://dl-cdn.alpinelinux.org/alpine/v3.14/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.14/community" >> /etc/apk/repositories
 setup-timezone -z US/Michigan
 setup-hostname {NAME}
-printf "y\n" | setup-disk -m data /dev/vdb
+blkid | grep -q "vdb"
+if [ $? -ne 0 ]; then
+    printf "y\n" | setup-disk -m data /dev/vdb
+else
+    mount -t ext4 /dev/vdb2 /var
+fi
 swapon /dev/vdb1
 /etc/init.d/swap start
 mkdir -p /var/cache/root
