babcc467fe29cc92cb5b87e42058eb817bd784c5
Sean Enck <enckse@voidedtech.com>
2022-03-13 17:12:09 -0400

    nixpkgs for home-manager

---
+++ nix/.config/nixpkgs/home.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+
+    home.username = "enck";
+    home.homeDirectory = "/home/enck";
+    home.stateVersion = "21.05";
+    programs.home-manager.enable = true;
+    home.packages = [
+        pkgs.sway
+        pkgs.firefox
+        pkgs.git
+        pkgs.mumble
+        pkgs.gnumake
+        pkgs.curl
+        pkgs.wget
+        pkgs.python3
+        pkgs.diff-so-fancy
+        pkgs.stow
+        pkgs.ripgrep
+        pkgs.pavucontrol
+        pkgs.gnupg
+        pkgs.keepassxc
+        pkgs.go
+        pkgs.gotools
+        pkgs.mblaze
+        pkgs.pamixer
+    ];
+}
