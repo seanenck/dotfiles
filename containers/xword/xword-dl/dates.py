07da26d08fdcca2e96d3e6694c02417ee995b65a
Sean Enck <enckse@voidedtech.com>
2022-08-07 13:54:45 -0400

    container defs

---
+++ containers/xword/xword-dl/dates.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Handle date generation."""
+import datetime
+import argparse
+
+
+def main():
+    """Program entry."""
+    p = argparse.ArgumentParser()
+    p.add_argument("--since", required=True)
+    p.add_argument("--until", required=True)
+    args = p.parse_args()
+    s = datetime.datetime.strptime(args.since, "%Y-%m-%d")
+    u = datetime.datetime.strptime(args.until, "%Y-%m-%d")
+    if u <= s:
+        print("invalid request, until <= since")
+        exit(1)
+    d = (u - s).days
+    start = 0
+    while start <= d:
+        print((u + datetime.timedelta(days=start * -1)).strftime("%Y-%m-%d"))
+        start += 1
+
+
+if __name__ == "__main__":
+    main()
