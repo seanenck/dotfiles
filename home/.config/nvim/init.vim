be961260cc945134459ad370118b813544ff52f0
Sean Enck <enckse@voidedtech.com>
2022-08-31 16:14:13 -0400

    all of it is back to lua now

--- home/.config/nvim/init.vim
+++
@@ -1,9 +0,0 @@
-lua require("base")
-
-" NOTE: somethings are easier in vimscript (for now at least)
-
-" Ale Settings
-
-" Airline settings
-let g:airline#extensions#tabline#enabled = 1
-let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -6,6 +6,7 @@ vim.fn['plug#end']()
 
 require("toggleterm").setup{}
 
+-- ALE settings
 function override_linters(extension, linters)
     vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         pattern = { "*." .. extension },
@@ -18,6 +19,9 @@ end
 vim.g.ale_set_highlights = 0
 vim.g.ale_sign_column_always = 1
 vim.g.ale_completion_enabled = 1
-
 override_linters("py", {["python"] = {"pylsp", "pycodestyle", "flake8", "pydocstyle"}})
 override_linters("go", {["go"] = {"gopls", "revive", "govet"}}) 
+
+-- Airline settings
+vim.g.airline_extensions = {"tabline"}
+vim.g.airline_extensions["tabline"] = {["formatter"] = "unique_tail_improved"}
