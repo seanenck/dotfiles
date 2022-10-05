1c54ec8332298c697f4efdb6e5ff67cc735b980a
Sean Enck <enckse@voidedtech.com>
2022-10-05 14:22:54 -0400

    migrated elsewhere

--- containers/resume/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source resume.env
-make ADDRESS="$HOME_ADDRESS" PHONE="$HOME_PHONE" EMAIL="$HOME_EMAIL"
-test -s bin/resume.pdf || exit 1
-if ! mv bin/resume.pdf /opt/$1.pdf; then
-    exit 1
-fi
-exit 0
