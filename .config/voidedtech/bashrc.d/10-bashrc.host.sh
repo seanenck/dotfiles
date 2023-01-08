3eb0498e494c9b12e467575fd9636c300560a8d1
Sean Enck <enckse@voidedtech.com>
2023-01-08 09:16:21 -0500

    let know about upstreams

--- .config/voidedtech/bashrc.d/10-bashrc.host.sh
+++ .config/voidedtech/bashrc.d/10-bashrc.host.sh
@@ -4,12 +4,16 @@ if [ -n "$SSH_CONNECTION" ]; then
     export LOCKBOX_CLIP_OSC52=yes
 fi
 
-_not-pushed() {
-    if ! git uncommitted --quiet; then
-        echo
-        echo "uncommitted:"
-        git uncommitted | cut -d " " -f 1 | sort -u | sed "s#$HOME/##g" | sed 's/^/  -> /g'
-        echo
-    fi
-}
-_not-pushed
+if ! git uncommitted --quiet; then
+    echo
+    echo "uncommitted:"
+    git uncommitted | cut -d " " -f 1 | sort -u | sed "s#$HOME/##g" | sed 's/^/  -> /g'
+    echo
+fi
+
+if ! upstreams check; then
+    echo
+    echo "upstreams:"
+    echo "  -> check for updates"
+    echo
+fi
