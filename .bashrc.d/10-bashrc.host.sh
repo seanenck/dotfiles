9542da7bd6a0d8532b5904847a1bd62cddd70cae
Sean Enck <enckse@voidedtech.com>
2023-01-09 08:45:18 -0500

    some quoting/cleanup

--- .bashrc.d/10-bashrc.host.sh
+++ .bashrc.d/10-bashrc.host.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -n "$SSH_CONNECTION" ]; then
-    export LOCKBOX_CLIP_OSC52=yes
-fi
-
 if ! git uncommitted --quiet; then
     echo
     echo "uncommitted:"
--- .bashrc.d/50-aliases.host.sh
+++ .bashrc.d/50-aliases.host.sh
@@ -3,7 +3,7 @@ advantage360() {
 	local cache
 	cache="$HOME/.cache/adv360"
 	if [ ! -d "$cache" ]; then
-		if ! git clone https://github.com/enckse/Adv360-Pro-ZMK $cache; then
+		if ! git clone https://github.com/enckse/Adv360-Pro-ZMK "$cache"; then
 			return
 		fi
 	fi
@@ -13,5 +13,5 @@ advantage360() {
 	if ! git -C "$cache" pull; then
 		return
 	fi
-	git -C "$cache" diff 0fb8e5824fee2fb11f263de745f5b1c0efbcd78a > $HOME/.config/voidedtech/adv360/mappings.patch
+	git -C "$cache" diff 0fb8e5824fee2fb11f263de745f5b1c0efbcd78a > "$HOME/.config/voidedtech/adv360/mappings.patch"
 }
