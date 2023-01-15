7704817de24339683325aaaba0a999fd455ca2c0
Sean Enck <enckse@voidedtech.com>
2023-01-15 08:34:51 -0500

    support a local repo

--- .bashrc.d/40-completions.host.sh
+++ .bashrc.d/40-completions.host.sh
@@ -4,6 +4,9 @@ _enter-toolbox() {
     if [ "$COMP_CWORD" -eq 1 ]; then
         cur=${COMP_WORDS[COMP_CWORD]}
         opts=$(enter-toolbox --list)
+        if [ -n "$opts" ]; then
+            opts="$opts --update"
+        fi
         COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
     fi
 }
