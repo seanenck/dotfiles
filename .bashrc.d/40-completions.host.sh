5d580e828631730ddac3792fc2f391fe62dee5f0
Sean Enck <enckse@voidedtech.com>
2023-01-21 09:10:56 -0500

    renaming back to enter-toolbox

--- .bashrc.d/40-completions.host.sh
+++ .bashrc.d/40-completions.host.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-_boxed() {
+_enter-toolbox() {
     local cur opts
     if [ "$COMP_CWORD" -eq 1 ]; then
         cur=${COMP_WORDS[COMP_CWORD]}
-        opts=$(boxed --list)
+        opts=$(enter-toolbox --list)
         COMPREPLY=( $(compgen -W "$opts --update" -- "$cur") )
     fi
 }
 
-complete -F _boxed -o bashdefault -o default boxed
+complete -F _enter-toolbox -o bashdefault -o default enter-toolbox
