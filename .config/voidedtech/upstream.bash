c30f05d113084c43157f40bf4ae06b2efbde1df5
Sean Enck <enckse@voidedtech.com>
2023-02-12 17:09:21 -0500

    deprecate isync, add revive

--- .config/voidedtech/upstream.bash
+++ .config/voidedtech/upstream.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 declare -A SOURCES=( \
   ["gopls"]="https://github.com/golang/tools" \
-  ["isync"]="https://git.code.sf.net/p/isync/isync" \
   ["staticcheck"]="https://github.com/dominikh/go-tools" \
   ["efmlsp"]="https://github.com/mattn/efm-langserver" \
   ["gofumpt"]="https://github.com/mvdan/gofumpt" \
+  ["revive"]="https://github.com/mgechev/revive" \
 )
 declare -a PLUGINS=( \
   "https://github.com/vim-airline/vim-airline" \
