12a2204dbc3b94ac3d212249682c67654571bcd5
Sean Enck <enckse@voidedtech.com>
2023-02-12 18:42:14 -0500

    upstreams should come from build area

--- .config/voidedtech/upstream.bash
+++ .config/voidedtech/upstream.bash
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-declare -A SOURCES=( \
-  ["gopls"]="https://github.com/golang/tools" \
-  ["staticcheck"]="https://github.com/dominikh/go-tools" \
-  ["efmlsp"]="https://github.com/mattn/efm-langserver" \
-  ["gofumpt"]="https://github.com/mvdan/gofumpt" \
-  ["revive"]="https://github.com/mgechev/revive" \
-)
 declare -a PLUGINS=( \
   "https://github.com/vim-airline/vim-airline" \
   "https://github.com/neovim/nvim-lspconfig" \
@@ -17,4 +10,3 @@ declare -a PLUGINS=( \
   "https://github.com/nvim-lua/plenary.nvim" \
 )
 export PLUGINS
-export SOURCES
