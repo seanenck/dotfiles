9ec0894e880f356c59c9f12b4be22d53a8fd796e
Sean Enck <enckse@voidedtech.com>
2023-02-17 11:51:09 -0500

    rename and move

--- .bashrc.d/99-aliases.all.sh
+++ .bashrc.d/99-aliases.all.sh
@@ -6,3 +6,11 @@ alias grep="rg"
 alias vi="$EDITOR"
 alias vim="$EDITOR"
 alias scp="rsync"
+
+unpack-rpm() {
+  if [ -z "$1" ]; then
+    echo "rpm required"
+    return
+  fi
+  rpm2cpio "$1" | cpio -idmv
+}
