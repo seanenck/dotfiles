93301e4a78884462404974498642901e296ed586
Sean Enck <enckse@voidedtech.com>
2023-03-05 10:37:13 -0500

    all tools build

--- .bashrc.d/40-completions.host.sh
+++ .bashrc.d/40-completions.host.sh
@@ -9,3 +9,14 @@ _enter-toolbox() {
 }
 
 complete -F _enter-toolbox -o bashdefault -o default enter-toolbox
+
+_tool-build() {
+  local cur opts
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    cur=${COMP_WORDS[COMP_CWORD]}
+    opts=$(tool-build --list)
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+  fi
+}
+
+complete -F _tool-build -o bashdefault -o default tool-build
