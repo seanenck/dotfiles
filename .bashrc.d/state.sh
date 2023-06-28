9cef90692911fcbaa6231fecd6312272164e0676
Sean Enck <enckse@voidedtech.com>
2023-06-28 13:08:46 -0400

    spacing

--- .bashrc.d/state.sh
+++ .bashrc.d/state.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo "state"
 echo "==="
+echo
 df -h / | tail -n +2 | awk '{print "disk usage: " $5}'
+echo
 if ! git uncommitted --quiet; then
-  echo
   echo "uncommitted:"
   git uncommitted | cut -d " " -f 1 | sort -u | sed "s#$HOME/##g" | sed 's/^/  -> /g'
   echo
