7423d8f8361a5190c2d5242f4cc3915aeaa6cf1a
Sean Enck <enckse@voidedtech.com>
2023-07-16 10:29:26 -0400

    pull disk this way

--- .bashrc.d/state.sh
+++ .bashrc.d/state.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+_disk() {
+  local f
+  echo "disk:"
+  for f in share /dev/vd; do
+    df -h 2>/dev/null | grep "^$f" | awk '{printf("%-15s%s\n", $1, $5)}' | sort -u | sed 's/^/  -> /g'
+  done
+}
+
+_disk
 echo
 if ! git uncommitted --quiet; then
   echo "uncommitted:"
