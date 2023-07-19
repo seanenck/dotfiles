5ca175e137b690d6f5dd716d1e9816cfe4ac932e
Sean Enck <enckse@voidedtech.com>
2023-07-19 08:58:46 -0400

    filter state a bit

--- .bashrc.d/state.sh
+++ .bashrc.d/state.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+_df() {
+  df -h 2>/dev/null | grep "^$1" | grep "$2" | awk '{printf("%-15s%s\n", $1, $5)}' | sort -u | sed 's/^/  -> /g'
+}
 _disk() {
-  local f
+  local f filter
   echo "disk:"
   for f in share /dev/vd; do
-    df -h 2>/dev/null | grep "^$f" | awk '{printf("%-15s%s\n", $1, $5)}' | sort -u | sed 's/^/  -> /g'
+    filter=""
+    if [ "$f" == "/dev/vd" ]; then
+      filter="$f[a-z][0-9]"
+    fi
+    _df "$f" "$filter"
   done
 }
 
