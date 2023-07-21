b4a64942af70400b35bda564c2378782a8fc82bc
Sean Enck <enckse@voidedtech.com>
2023-07-21 18:01:11 -0400

    restore lb here

--- linux/.bashrc.d/go.sh
+++ linux/.bashrc.d/go.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 export GOPATH="$HOME/.cache/go"
 export GOFLAGS="-ldflags=-linkmode=external -trimpath -buildmode=pie -mod=readonly -modcacherw -buildvcs=false"
-
-macos-build() {
-  GOOS=darwin GOFLAGS=$(echo "$GOFLAGS" | sed 's/-buildmode=pie //g') $@
-}
