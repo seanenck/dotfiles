3d55b2bbab51831fff91f05e8f4320cc9dccf3f4
Sean Enck <enckse@voidedtech.com>
2023-07-21 11:38:21 -0400

    not running in alpine anymore

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 export GOPATH="$HOME/.cache/go"
 export GOFLAGS="-ldflags=-linkmode=external -trimpath -buildmode=pie -mod=readonly -modcacherw -buildvcs=false"
+
+macos-build() {
+  export GOOS=darwin
+  export GOFLAGS=$(echo "$GOFLAGS" | sed 's/-buildmode=pie //g')
+  $@
+}
