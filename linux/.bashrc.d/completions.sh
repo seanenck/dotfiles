81289e60e75c46778583b0fbc7dd89b1e7476dd1
Sean Enck <enckse@voidedtech.com>
2023-08-02 07:41:30 -0400

    apk completions

--- linux/.bashrc.d/completions.sh
+++ linux/.bashrc.d/completions.sh
@@ -7,3 +7,14 @@ _git_oclone() {
     COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
   fi
 }
+
+_apk() {
+  local opts cur
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    cur=${COMP_WORDS[COMP_CWORD]}
+    opts=$(echo "add del fix update upgrade cache info list dot policy search index fetch manifest verify audit stats version" | tr ' ' '\n')
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+  fi
+}
+
+complete -F _apk -o bashdefault apk
