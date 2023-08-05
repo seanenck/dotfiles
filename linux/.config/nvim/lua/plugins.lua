a8a637b9d99c77366f66986ce9a0ea3f5ad35c4b
Sean Enck <enckse@voidedtech.com>
2023-08-05 09:24:12 -0400

    fixing some ale oddities

--- linux/.config/nvim/lua/plugins.lua
+++ linux/.config/nvim/lua/plugins.lua
@@ -3,21 +3,13 @@ vim.g.airline_extensions = {"tabline"}
 vim.g.airline_extensions["tabline"] = {["formatter"] = "unique_tail_improved"}
 
 -- ale
-function override_linters(extension, fixers, linters)
-    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-        pattern = { "*." .. extension },
-        callback = function()
-            if linters ~= nil then
-                vim.g.ale_linters = linters
-            end
-            if fixers ~= nil then
-                vim.g.ale_fixers = fixers
-            end
-        end
-    })
+function ale_handle_fix(ft)
     vim.api.nvim_create_autocmd({ "BufWrite" }, {
-        pattern = { "*." .. extension },
+        pattern = { "*" },
         callback = function()
+            if vim.bo.filetype ~= ft then
+                return
+            end
             vim.api.nvim_exec("autocmd User ALEFixPost let g:override_alefix=0", false)
             vim.g.override_alefix = 1
             vim.api.nvim_exec(":ALEFix", false)
@@ -32,8 +24,16 @@ end
 vim.g.ale_set_highlights = 0
 vim.g.ale_sign_column_always = 1
 vim.g.ale_completion_enabled = 1
-override_linters("go", {["go"] = {"gofumpt"}}, {["go"] = {"gopls", "govet", "staticcheck", "revive"}}) 
-override_linters("pl", {["perl"] = {"perltidy"}}, {["perl"] = {"perl", "perlcritic"}})
+vim.g.ale_linters = {
+    ["go"] = {"gopls", "govet", "staticcheck", "revive"},
+    ["perl"] = {"perl", "perlcritic"},
+}
+vim.g.ale_fixers = {
+    ["go"] = {"gofumpt"},
+    ["perl"] = {"perltidy"},
+}
+ale_handle_fix("go")
+ale_handle_fix("perl")
 vim.g.ale_go_staticcheck_options = '-checks all'
 
 -- term
