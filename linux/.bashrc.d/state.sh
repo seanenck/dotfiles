4ce23ce34f6a0b92d09a8b11af0b460346cb0973
Sean Enck <enckse@voidedtech.com>
2023-08-05 10:42:15 -0400

    really want host and devices

--- linux/.bashrc.d/state.sh
+++ linux/.bashrc.d/state.sh
@@ -3,15 +3,9 @@ _df() {
   df -h 2>/dev/null | grep "^$1" | grep "$2" | awk '{printf("%-15s%s\n", $1, $5)}' | sort -u | sed 's/^/  -> /g'
 }
 _disk() {
-  local f filter
   echo "disk:"
-  for f in share /dev/vd; do
-    filter=""
-    if [ "$f" == "/dev/vd" ]; then
-      filter="$f[a-z][0-9]"
-    fi
-    _df "$f" "$filter"
-  done
+  _df "share" "" | sed "s/share/host /g"
+  _df "/dev/vd" "/dev/vd[a-z][0-9]"
 }
 
 _disk
