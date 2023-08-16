17d9aab9edaf4d3f7c8cbbc33ec8ea1d19c711f9
Sean Enck <enckse@voidedtech.com>
2023-08-16 13:23:47 -0400

    rebuilding as host setup

--- linux/.bashrc.d/state.sh
+++ linux/.bashrc.d/state.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-_df() {
-  df -h 2>/dev/null | grep "^$1" | grep "$2" | awk '{printf("%-15s%s\n", $1, $5)}' | sort -u | sed 's/^/  -> /g'
-}
-_disk() {
-  echo "disk:"
-  _df "share" "" | sed "s/share/host /g"
-  _df "/dev/vd" "/dev/vd[a-z][0-9]"
-}
-
-_disk
-echo
 if [ -n "$HAS_PERL" ]; then
   git uncommitted
 fi
