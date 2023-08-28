a10e0f781f6e58c45996e5c7ea3e4057d30895a4
Sean Enck <enckse@voidedtech.com>
2023-08-28 18:54:23 -0400

    preserve undo for formatting

--- linux/.config/nvim/lua/plugins.lua
+++ linux/.config/nvim/lua/plugins.lua
@@ -101,6 +101,18 @@ require('guard').setup({
     lsp_as_default_formatter = false,
 })
 
+function _G.preserve(cmd)
+    local cmd = string.format('keepjumps keeppatterns execute %q', cmd)
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_command(cmd)
+    local lastline = vim.fn.line('$')
+    if line > lastline then
+        line = lastline
+    end
+    vim.api.nvim_win_set_cursor(0, {line , col})
+end
+map('n', 'u', ':lua preserve(":undo")<cr>', opts)
+
 -- mini completion
 local keys = {
   ['cr']        = vim.api.nvim_replace_termcodes('<CR>', true, true, true),
