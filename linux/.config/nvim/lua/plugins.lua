1d5e7cb4d03cccf3ee4ee0b162f9724034cdb908
Sean Enck <enckse@voidedtech.com>
2023-08-28 18:13:29 -0400

    remap completion to right  arrow

--- linux/.config/nvim/lua/plugins.lua
+++ linux/.config/nvim/lua/plugins.lua
@@ -101,5 +101,23 @@ require('guard').setup({
 })
 
 -- mini completion
-vim.api.nvim_command('inoremap <silent><expr> <Right> pumvisible() ? "<C-n>" : "<Right>"')
+local keys = {
+  ['cr']        = vim.api.nvim_replace_termcodes('<CR>', true, true, true),
+  ['ctrl-y']    = vim.api.nvim_replace_termcodes('<C-y>', true, true, true),
+  ['ctrl-y_cr'] = vim.api.nvim_replace_termcodes('<C-y><CR>', true, true, true),
+}
+_G.cr_action = function()
+  if vim.fn.pumvisible() ~= 0 then
+    -- If popup is visible, confirm selected item or add new line otherwise
+    local item_selected = vim.fn.complete_info()['selected'] ~= -1
+    return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
+  else
+    -- If popup is not visible, use plain `<CR>`. You might want to customize
+    -- according to other plugins. For example, to use 'mini.pairs', replace
+    -- next line with `return require('mini.pairs').cr()`
+    return keys['cr']
+  end
+end
+
+vim.keymap.set('i', '<Right>', 'v:lua._G.cr_action()', { expr = true })
 require('mini.completion').setup()
