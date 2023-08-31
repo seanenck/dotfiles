b729831c17f50222b5b80cf3b1cb2766274d0929
Sean Enck <enckse@voidedtech.com>
2023-08-31 18:01:03 -0400

    moving things around to get away from persistent .cache

--- linux/.bashrc.d/state.sh
+++ linux/.bashrc.d/state.sh
@@ -4,8 +4,9 @@ _df() {
 }
 _disk() {
   echo "disk:"
-  _df "home" "" | sed "s/share/host /g"
+  _df "workspace" "" | sed "s/share/host /g"
   _df "/dev/vd" "/dev/vd[a-z][0-9]"
+  _df "tmpfs" "/home"
 }
 
 _disk
