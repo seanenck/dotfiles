dbb5ca52141ae68df9810f6ae5faa3097c7d4960
Sean Enck <enckse@voidedtech.com>
2023-11-17 11:27:46 -0500

    support only moving windows on the primary screen

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,21 +1,24 @@
 hs.hotkey.bind({'cmd','ctrl'}, 'space', function()
     local windows = hs.window.orderedWindows()
-    local screen = windows[1]:screen():frame()
+    local base = windows[1]:screen()
+    local screen = base:frame()
     local nOfSpaces = #windows > 1 and #windows - 1 or 1
 
     local xMargin = screen.w / 20 -- unused horizontal margin
     local yMargin = 20            -- unused vertical margin
     local spacing = 100           -- the visible margin for each window
-
+    local id = base:id()
     for i, win in ipairs(windows) do
-        local offset = (i - 1) * spacing
-        local rect = {
-            x = xMargin + offset,
-            y = screen.y + yMargin + offset,
-            w = screen.w - (2 * xMargin) - (nOfSpaces * spacing),
-            h = screen.h - (2 * yMargin) - (nOfSpaces * spacing),
-        }
-        win:setFrame(rect)
+        if win:screen():id() == id then
+            local offset = (i - 1) * spacing
+            local rect = {
+                x = xMargin + offset,
+                y = screen.y + yMargin + offset,
+                w = screen.w - (2 * xMargin) - (nOfSpaces * spacing),
+                h = screen.h - (2 * yMargin) - (nOfSpaces * spacing),
+            }
+            win:setFrame(rect)
+        end
     end
 end)
 
