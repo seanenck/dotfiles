0003642be6a1df7e553f91369c1867a05ce5e8d6
Sean Enck <enckse@voidedtech.com>
2024-01-17 14:02:41 -0500

    i don't actually want snippets

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,17 +6,11 @@ vim.g.airline_extensions["tabline"] = {["formatter"] = "unique_tail_improved"}
 local cmp = require'cmp'
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
     mapping = cmp.mapping.preset.insert({
         ['<Right>'] = cmp.mapping.confirm({ select = false }),
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
     }, {
         { name = 'buffer' },
     })
