d4495d69de33e6f3646f246ad6bab298b0ebf882
Sean Enck <enckse@voidedtech.com>
2024-03-21 21:58:16 -0400

    wrap this in []

--- linux/.config/fish/functions/fish_prompt.fish
+++ linux/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 function fish_prompt -d "Write out the prompt"
-    printf '%s%s@%s %s%s%s > ' "$(git uncommitted --pwd 2>/dev/null)" $USER $(hostname | cut -d "." -f 1) \
+    printf '[%s%s@%s %s%s%s]> ' "$(git uncommitted --pwd 2>/dev/null)" $USER $(hostname | cut -d "." -f 1) \
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
