a22028be994efac2bc862830514bb0b2bc8261a5
Sean Enck <enckse@voidedtech.com>
2024-03-22 11:39:08 -0400

    change casing

--- dev/.config/fish/conf.d/vars.fish
+++ dev/.config/fish/conf.d/vars.fish
@@ -2,6 +2,6 @@ set PATH $HOME/.local/bin $PATH
 set -U EDITOR nvim
 set -U VISUAL $EDITOR
 set -x DELTA_PAGER "less -c -X"
-set -x GOPATH "$HOME/Library/go"
+set -x GOPATH "$HOME/Library/Go"
 set -x GOFLAGS "-ldflags=-linkmode=external -trimpath -buildmode=pie -mod=readonly -modcacherw -buildvcs=false"
 set -x DOTFILES_PROFILE dev 
