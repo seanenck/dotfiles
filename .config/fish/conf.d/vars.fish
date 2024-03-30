13d30f84f383f3cba0ffaa0a09f79ca4d3d92322
Sean Enck <enckse@voidedtech.com>
2024-03-30 01:08:22 -0400

    deprecated

--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -4,4 +4,3 @@ set -U VISUAL $EDITOR
 set -x DELTA_PAGER "less -c -X"
 set -x GOPATH "$HOME/Library/Go"
 set -x GOFLAGS "-ldflags=-linkmode=external -trimpath -buildmode=pie -mod=readonly -modcacherw -buildvcs=false"
-set -x DOTFILES_PROFILE dev 
