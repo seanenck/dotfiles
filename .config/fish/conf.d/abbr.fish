33491935d1e62cf1de97f717a3a2a9adde04ccf6
Sean Enck <enckse@voidedtech.com>
2024-04-03 19:12:01 -0400

    handle multiple cases for uname

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,4 +1,9 @@
-abbr -a cat bat
+switch (uname)
+    case Darwin
+        abbr -a cat bat
+    case Linux
+        abbr -a cat batcat
+end
 abbr -a grep rg
 abbr -a vi $EDITOR
 abbr -a vim $EDITOR
--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -2,6 +2,11 @@ fish_add_path -gP "$HOME/.local/bin";
 set -U EDITOR nvim
 set -U VISUAL $EDITOR
 set -x DELTA_PAGER "less -c -X"
-set -x GOPATH "$HOME/Library/Go"
+switch (uname)
+    case Darwin
+        set -x GOPATH "$HOME/Library/Go"
+    case Linux
+        set -x GOPATH "$HOME/.cache/go"
+end
 set -x GOFLAGS "-ldflags=-linkmode=external -trimpath -buildmode=pie -mod=readonly -modcacherw -buildvcs=false"
 set -x SECRETS "$HOME/Git/secrets"
