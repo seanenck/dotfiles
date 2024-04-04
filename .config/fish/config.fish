2306351f786a026d58a2af3fba4b8e4ce382eb20
Sean Enck <enckse@voidedtech.com>
2024-04-04 19:50:01 -0400

    Revert "dirty state"
    This reverts commit 57904e7398bb10b1d7522a5688125f162bbf3d24.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -44,7 +44,6 @@ if status is-interactive
     end
     set -g __fish_git_prompt_show_informative_status 1
     set -g __fish_git_prompt_hide_untrackedfiles 0
-    set -g __fish_git_prompt_showdirtystate 1
     
     set -g __fish_git_prompt_color_branch magenta bold
     set -g __fish_git_prompt_showupstream "informative"
@@ -58,5 +57,11 @@ if status is-interactive
     set -g __fish_git_prompt_char_conflictedstate "✖"
     set -g __fish_git_prompt_char_cleanstate "✔"
     
+    set -g __fish_git_prompt_color_dirtystate blue
+    set -g __fish_git_prompt_color_stagedstate yellow
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    set -g __fish_git_prompt_color_cleanstate green bold
+
     voidedtech startup
 end
