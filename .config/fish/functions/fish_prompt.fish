8326f56ea717edfa0b044d88254c41c6dcf0db5e
Sean Enck <enckse@voidedtech.com>
2024-04-08 18:00:22 -0400

    fixup fish+uncommit

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,6 +5,6 @@ function fish_prompt -d "Write out the prompt"
         set -f display $(cat /run/.containerenv | grep '^name=' | cut -d "=" -f 2 | sed 's/"//g')
     end
     set -f usepwd $(basename (prompt_pwd))
-    printf '[%s%s@%s %s%s%s]> ' "$(git uncommitted pwd 2>/dev/null)" $USER (set_color purple)$display(set_color normal) \
+    printf '[%s%s@%s %s%s%s]> ' "$(git uncommitted --pwd 2>/dev/null)" $USER (set_color purple)$display(set_color normal) \
         (set_color $fish_color_cwd) $usepwd (set_color normal)
 end
