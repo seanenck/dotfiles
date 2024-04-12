5d2226b5530059c35cb027bc475f859e81d97a71
Sean Enck <enckse@voidedtech.com>
2024-04-11 21:44:19 -0400

    restore local bin path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@ set -g VISUAL $EDITOR
 set -x SECRETS "$HOME/Env/secrets"
 set -x DELTA_PAGER "less -c -X"
 set -l local_bin "$HOME/.local/bin"
+if test -d "$local_bin"
+    fish_add_path -gP "$local_bin";
+end
 
 if test -e /run/.containerenv
     switch (cat /run/.containerenv | grep "^name=" | cut -d "=" -f 2- | sed 's/"//g')
@@ -12,9 +15,6 @@ if test -e /run/.containerenv
             set -x GOFLAGS "-ldflags=-linkmode=external -trimpath -buildmode=pie -mod=readonly -modcacherw -buildvcs=false"
     end
 else
-    if test -d "$local_bin"
-        fish_add_path -gP "$local_bin";
-    end
     if test -x "$local_bin/voidedtech"
         set -f do_startup 1
     end
