a2f9d20f6dc86c137667ccd51f076fc3a487ad29
Sean Enck <enckse@voidedtech.com>
2024-04-11 20:24:14 -0400

    move this to also be defined in this function

--- .config/fish/functions/gotooling.fish
+++ .config/fish/functions/gotooling.fish
@@ -1,4 +1,15 @@
-if ! test -e /run/.containerenv
+if test -e /run/.containerenv
+    if set -q GOPATH
+        function gotooling
+            for tool in (cat "$HOME/.config/voidedtech/gotools")
+                echo "installing: $tool"
+                if ! go install "$tool"
+                    echo "  -> failed"
+                end
+            end
+        end
+    end
+else
     function gotooling
         toolbox run -c go fish -c "gotooling"
     end
---
+++ .config/fish/functions/neovim-plugins.fish
@@ -0,0 +1,19 @@
+if ! test -e /run/.containerenv
+    function neovim-plugins
+        set -l plugins "$HOME/.config/nvim/pack/plugins/start"
+        for plugin in (cat "$HOME/.config/voidedtech/neovim-plugins")
+            set -l name (basename "$plugin")
+            set -l clone "$plugins/$name"
+            echo "updating plugin: $name"
+            if test -d "$clone"
+                if ! git -C "$clone" pull origin (git -C "$clone" rev-parse --abbrev-ref HEAD)
+                    echo "  -> failed to update"
+                end
+            else
+                if ! git clone "$plugin" "$clone" --single-branch
+                    echo "  -> failed to clone"
+                end
+            end
+        end
+    end
+end
