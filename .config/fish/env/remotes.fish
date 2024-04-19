cd6ecc0f4acc086b676f3282b23702f05f3f6853
Sean Enck <enckse@voidedtech.com>
2024-04-18 20:21:08 -0400

    manually manage

--- .config/fish/env/remotes.fish
+++ .config/fish/env/remotes.fish
@@ -4,7 +4,11 @@ switch (uname)
             set -l remote_file "$HOME/.local/voidedtech/remotes"
             set -l remote_last "$remote_file.last"
             touch "$remote_file" "$remote_last"
-            for remote in https://gitlab.alpinelinux.org/alpine/aports https://github.com/kovidgoyal/kitty
+            for remote in https://gitlab.alpinelinux.org/alpine/aports \
+                          https://github.com/kovidgoyal/kitty \
+                          https://github.com/fish-shell/fish-shell \
+                          https://github.com/dandavison/delta \
+                          https://github.com/neovim/neovim
                 echo "getting: $remote"
                 set -l name $(basename "$remote")
                 git ls-remote --tags "$remote" 2>/dev/null | awk '{print $2}' | grep -v '{}' | rev | cut -d "/" -f 1 | rev | sed "s#^#$name #g" >> "$remote_file"
