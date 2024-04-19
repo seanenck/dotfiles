99fcdf4fd564c3b4378d7e5c8a281f456b607537
Sean Enck <enckse@voidedtech.com>
2024-04-19 17:56:07 -0400

    change prompt color based on uname

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,10 +1,11 @@
 #!/usr/bin/env fish
 function fish_prompt -d "Write out the prompt"
-    set -f display $hostname
-    if test -e "$HOME/.host"
-        set -f display $(cat "$HOME/.host")
+    set -f display purple
+    switch (uname)
+        case Linux
+            set -f display blue
     end
     set -f usepwd $(basename (prompt_pwd))
-    printf '[%s%s@%s %s%s%s]> ' "$(git uncommitted --pwd 2>/dev/null)" $USER (set_color purple)$display(set_color normal) \
+    printf '[%s%s@%s %s%s%s]> ' "$(git uncommitted --pwd 2>/dev/null)" $USER (set_color $display)$hostname(set_color normal) \
         (set_color $fish_color_cwd) $usepwd (set_color normal)
 end
