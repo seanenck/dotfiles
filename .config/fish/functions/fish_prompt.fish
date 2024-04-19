8ae13e519cefee3e64756b43b360b50462c3fbb0
Sean Enck <enckse@voidedtech.com>
2024-04-19 16:39:04 -0400

    override host in some cases

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/env fish
 function fish_prompt -d "Write out the prompt"
     set -f display $hostname
-    if test -e /run/.containerenv
-        set -f display $(cat /run/.containerenv | grep '^name=' | cut -d "=" -f 2 | sed 's/"//g')
+    if test -e "$HOME/.host"
+        set -f display $(cat "$HOME/.host")
     end
     set -f usepwd $(basename (prompt_pwd))
     printf '[%s%s@%s %s%s%s]> ' "$(git uncommitted --pwd 2>/dev/null)" $USER (set_color purple)$display(set_color normal) \
