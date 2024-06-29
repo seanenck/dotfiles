41ab3bb411998db678b1defc2a47514bffcfeb4e
Sean Enck <sean@ttypty.com>
2024-06-29 18:46:16 -0400

    new pubkey for signing

--- .config/fish/env/gotooling.fish
+++ .config/fish/env/gotooling.fish
@@ -1,6 +1,6 @@
 if set -q GOPATH
     function gotooling
-        for tool in (cat "$HOME/.config/voidedtech/gotools")
+        for tool in (cat "$HOME/.config/etc/gotools")
             echo "installing: $tool"
             if ! go install "$tool"
                 echo "  -> failed"
