099752d27138a5efe727f460a4ab767234ec0cba
Sean Enck <sean@ttypty.com>
2024-07-05 18:01:32 -0400

    bundle brew during update

--- .config/fish/env/update-system.fish
+++ .config/fish/env/update-system.fish
@@ -2,4 +2,8 @@ function update-system
     neovim-plugins
     gotooling
     remotes
+    set -l brewfile "$HOME/.local/state/Brewfile"
+    rm -f "$brewfile"
+    cd $(dirname "$brewfile")
+    brew bundle dump
 end
