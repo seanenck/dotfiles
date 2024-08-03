8ee655859e6eb4ff981bacdc130128fcdebc1485
Sean Enck <sean@ttypty.com>
2024-08-03 19:18:49 -0400

    eliminating some lua setup for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,51 @@
 -- Plugins
 require("options")
-require("helpers")
 require("plugins")
 require("autocmds")
+
+local function map(mode, shortcut, command)
+    vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+end
+
+local function nmap(shortcut, command)
+    map('n', shortcut, command)
+end
+
+local function imap(shortcut, command)
+    map('i', shortcut, command)
+end
+
+local function vmap(shortcut, command)
+    map('v', shortcut, command)
+end
+
+local function nop(vals)
+    for i, val in pairs(vals) do
+        for idx, fxn in pairs({nmap, imap, vmap}) do
+            fxn(val, '<Nop>')
+        end
+    end
+end
+
 
 if vim.version().major == 0 and vim.version().minor >= 10 then
     vim.cmd 'colorscheme vim'
 end
 
+-- Disable help
+nop({"<F1>"})
 -- Mouse, disable paste on middle click
 for _, mouseMiddle in ipairs({"", "2-", "3-", "4-"}) do
-    mapall("<" .. mouseMiddle .. "MiddleMouse>", '<Nop>')
+    nop({"<" .. mouseMiddle .. "MiddleMouse>"})
 end
 
--- Disable help
-mapall("<F1>", "<Nop>")
--- Disable macros
+-- disable macros
 nmap("q", "<Nop>")
 
 -- Disable shifted cursor keys, ctrl jklh cursor, and q
 -- Disable ctrl jklh cursors
-disableall({"<S-Up>", "<S-Down>", "<S-Right>", "<S-Left>"})
-disableall({"<C-h>", "<C-l>", "<C-q>"})
+nop({"<S-Up>", "<S-Down>", "<S-Right>", "<S-Left>",
+     "<C-h>", "<C-l>", "<C-q>"})
 
 -- Buffer/split movements
 nmap("<Tab>", ":bnext<CR>")
@@ -29,8 +53,7 @@ nmap("<S-Tab>", ":bprevious<CR>")
 nmap("<C-w>", ":bprevious <BAR> bd #<CR>")
 nmap("<S-l>", ":wincmd l<CR>")
 nmap("<S-h>", ":wincmd h<CR>")
-local move_maps = { ["gl"] = "$", ["gh"] = "^", ["gk"] = "gg", ["gj"] = "G"}
-for key, command in pairs(move_maps) do
+for key, command in pairs({ ["gl"] = "$", ["gh"] = "^", ["gk"] = "gg", ["gj"] = "G"}) do
     nmap(key, command)
     vmap(key, command)
 end
