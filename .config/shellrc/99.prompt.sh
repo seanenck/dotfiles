45471a3c6c0f2a55574757b50a14a45cdfd3cf99
Sean Enck <sean@ttypty.com>
2024-09-23 17:24:45 -0400

    enable git status in macos

--- .config/shellrc/99.prompt.sh
+++ .config/shellrc/99.prompt.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 if command -v git-uncommitted >/dev/null; then
-  PS1="[\u@\[\e[93m\]\h\[\e[0m\]:\W]$ "
-  PS1="\$(git uncommitted --mode pwd 2>/dev/null)$PS1"
+  if [ "$SHELL" = "/bin/bash" ]; then
+    PS1="[\u@\[\e[93m\]\h\[\e[0m\]:\W]$ "
+    PS1="\$(git uncommitted --mode pwd 2>/dev/null)$PS1"
+  fi
   
   git-uncommitted --mode motd
 fi
