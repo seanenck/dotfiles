5894d6d7d3cf65777e3b3bdcb73e46674ec4fe07
Sean Enck <sean@ttypty.com>
2024-10-09 15:51:31 -0400

    rework shell scripts/deployment to be all optional

--- .config/shellrc/75.ssh.sh
+++ .config/shellrc/75.ssh.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 if command -v ssh-agent > /dev/null; then
   LOCAL_STATE="$HOME/.local/state"
-  mkdir -p "$LOCAL_STATE"
-  
-  SSH_AGENT_ENV="$LOCAL_STATE/ssh-agent.env"
-  if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > "$SSH_AGENT_ENV"
+  SSH_FILES="$HOME/.ssh"
+  if [ -e "$SSH_FILES/allowed_signers" ]; then
+    SSH_AGENT_ENV="$LOCAL_STATE/ssh-agent.env"
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+      ssh-agent > "$SSH_AGENT_ENV"
+    fi
+    export SSH_AUTH_SOCK="$LOCAL_STATE/ssh-agent.socket"
+    if [ ! -f "$SSH_AUTH_SOCK" ]; then
+      source "$SSH_AGENT_ENV" > /dev/null
+    fi
+    for FILE in "$SSH_FILES/"*.privkey; do
+      ssh-add "$FILE" > /dev/null 2>&1
+    done
   fi
-  export SSH_AUTH_SOCK="$LOCAL_STATE/ssh-agent.socket"
-  if [ ! -f "$SSH_AUTH_SOCK" ]; then
-    source "$SSH_AGENT_ENV" > /dev/null
-  fi
-  for FILE in "$HOME/.ssh/"*.privkey; do
-    ssh-add "$FILE" > /dev/null 2>&1
-  done
   
-  unset LOCAL_STATE SSH_AGENT_ENV FILE
+  unset LOCAL_STATE SSH_AGENT_ENV FILE SSH_FILES
 fi
