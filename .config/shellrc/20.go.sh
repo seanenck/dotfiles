85aace94caf9df05489dc85379713c646bdfbcae
Sean Enck <sean@ttypty.com>
2024-10-14 10:15:26 -0400

    cleaning up, use bd

--- .config/shellrc/20.go.sh
+++ .config/shellrc/20.go.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-HAS_GO=0
 if command -v go >/dev/null; then
-  export GOTOOLCHAIN=local
-  HAS_GO=1
-fi
-for DIR in "Library" ".cache"; do
-  DIR="$HOME/$DIR"
-  if [ -d "$DIR" ]; then
-    GOPATH="$DIR/go"
-    if [ "$HAS_GO" -eq 1 ]; then
-      export GOPATH
+  for DIR in "Library" ".cache"; do
+    DIR="$HOME/$DIR"
+    if [ -d "$DIR" ]; then
+      export GOPATH="$DIR/go"
+      export PATH="$GOPATH/bin:$PATH"
     fi
-    export PATH="$GOPATH/bin:$PATH"
-  fi
-done
-unset DIR HAS_GO
+  done
+  unset DIR
+fi
