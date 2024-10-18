e8c905e55100bda479e8e98343eee6b6c4245b4e
Sean Enck <sean@ttypty.com>
2024-10-18 08:10:26 -0400

    eliminate obnoxious dev experience

--- .config/shellrc/20.go.sh
+++ .config/shellrc/20.go.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
+for DIR in "Library" ".cache"; do
+  DIR="$HOME/$DIR"
+  if [ -d "$DIR" ]; then
+    export GOPATH="$DIR/go"
+  fi
+done
+unset DIR
 if command -v go >/dev/null; then
-  for DIR in "Library" ".cache"; do
-    DIR="$HOME/$DIR"
-    if [ -d "$DIR" ]; then
-      export GOPATH="$DIR/go"
-      export PATH="$GOPATH/bin:$PATH"
-    fi
-  done
   golint() {
     if [ ! -e go.mod ]; then
       echo "cowardly refusing to run outside go.mod root"
@@ -17,5 +17,4 @@ if command -v go >/dev/null; then
     staticcheck -checks all -debug.run-quickfix-analyzers ./... | sed 's#^#staticcheck: #g'
     gofumpt -l -extra $(find . -type f -name "*.go") | sed 's#^#gofumpt:     #g'
   }
-  unset DIR
 fi
