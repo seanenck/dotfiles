3cd59168032431fd28cfd81e10c00018274d13c5
Sean Enck <sean@ttypty.com>
2024-10-17 22:25:31 -0400

    add just, completions should come from blap

--- .config/shellrc/99.completions.sh
+++ .config/shellrc/99.completions.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 COMPS="$HOME/.config/shellrc/completions"
 if [ -d "$COMPS" ]; then
-  for FILE in lb blap; do
-    if ! command -v "$FILE" > /dev/null; then
-      continue
-    fi
-    BASE="$COMPS/$FILE"
-    if [ -e "$BASE" ]; then
-      continue
-    fi
-    $FILE completions > "$BASE"
-  done
   for FILE in "$COMPS/"*; do
-    BASE=$(basename "$FILE")
+    BASE=$(basename "$FILE" | cut -d "." -f 1)
     if command -v "$BASE" > /dev/null; then
       source "$FILE"
     fi
