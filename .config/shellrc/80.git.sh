d0f312b5039b7b7900e25c6e0219b284f9721b85
Sean Enck <sean@ttypty.com>
2024-10-21 17:11:43 -0400

    too many items dumping into 99..

--- .config/shellrc/80.git.sh
+++ .config/shellrc/80.git.sh
@@ -4,3 +4,7 @@ if ! command -v delta > /dev/null; then
     alias $(basename "$FILE")=""
   done
 fi
+if command -v git-dotfiles >/dev/null; then
+  export GIT_DOTFILES_ROOT="$HOME/Env/dotfiles"
+  export GIT_DOTFILES_DIFF="delta --paging=never"
+fi
--- .config/shellrc/99.prompt.sh
+++ .config/shellrc/99.prompt.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-if command -v git-dotfiles >/dev/null; then
-  export GIT_DOTFILES_ROOT="$HOME/Env/dotfiles"
-fi
 USE_HOST="\h"
 if [ -n "$CONTAINER_NAME" ]; then
   USE_HOST="$CONTAINER_NAME"
@@ -12,8 +9,5 @@ if command -v git-uncommitted >/dev/null; then
   if [ "$SHELL" = "/bin/bash" ]; then
     PS1="\$(git uncommitted --mode pwd 2>/dev/null)$PS1"
   fi
-fi
-if command -v git-motd >/dev/null; then
-  git motd
 fi
 unset USE_HOST
