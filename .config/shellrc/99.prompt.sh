c4ad2e017c8cfc5fece80402d50be6e4cc6bcbf3
Sean Enck <sean@ttypty.com>
2024-10-21 16:53:53 -0400

    rework blap output dirs, fixup git tooling

--- .config/shellrc/99.prompt.sh
+++ .config/shellrc/99.prompt.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-HAS_HEADER=0
-HEADER="state
-==="
-
 if command -v git-dotfiles >/dev/null; then
   export GIT_DOTFILES_ROOT="$HOME/Env/dotfiles"
-  DIFF=$(git dotfiles motd)
-  if [ -n "$DIFF" ]; then
-    echo "$HEADER"
-    echo "[dotfiles]"
-    echo "$DIFF"
-    HAS_HEADER=1
-  fi
 fi
 USE_HOST="\h"
 if [ -n "$CONTAINER_NAME" ]; then
@@ -23,18 +12,8 @@ if command -v git-uncommitted >/dev/null; then
   if [ "$SHELL" = "/bin/bash" ]; then
     PS1="\$(git uncommitted --mode pwd 2>/dev/null)$PS1"
   fi
-  
-  DIFF=$(git-uncommitted --mode motd)
-  if [ -n "$DIFF" ]; then
-    if [ "$HAS_HEADER" -eq 0 ]; then
-      echo "$HEADER"
-    fi
-    echo "[uncommitted]"
-    echo "$DIFF"
-    HAS_HEADER=1
-  fi
 fi
-if [ "$HAS_HEADER" -ne 0 ]; then
-  echo
+if command -v git-motd >/dev/null; then
+  git motd
 fi
-unset USE_HOST HAS_HEADER HEADER DIFF
+unset USE_HOST
