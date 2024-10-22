b62b8adb438c91a32a4449bf9637591713c6bd3d
Sean Enck <sean@ttypty.com>
2024-10-21 21:14:17 -0400

    enable delta for diff only if it is there

--- .config/shellrc/80.git.sh
+++ .config/shellrc/80.git.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+HAS_DELTA=1
 if ! command -v delta > /dev/null; then
+  HAS_DELTA=0
   for FILE in /usr/bin/git*; do
     alias $(basename "$FILE")=""
   done
 fi
 if command -v git-dotfiles >/dev/null; then
   export GIT_DOTFILES_ROOT="$HOME/Env/dotfiles"
-  export GIT_DOTFILES_DIFF="delta --paging=never"
+  if [ "$HAS_DELTA" -eq 1 ]; then
+    export GIT_DOTFILES_DIFF="delta --paging=never"
+  fi
 fi
 if command -v git-uncommitted >/dev/null; then
   export GIT_UNCOMMITTED="$HOME/Workspace $HOME/Env"
 fi
+unset HAS_DELTA
