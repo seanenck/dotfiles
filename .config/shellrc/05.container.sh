34414205fc2b46ba65ec5814396a9c1500124e75
Sean Enck <sean@ttypty.com>
2024-10-23 19:27:04 -0400

    move path logic for container/host

--- .config/shellrc/05.container.sh
+++ .config/shellrc/05.container.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 FILE="/run/.containerenv"
 if [ -e "$FILE" ]; then
-  NAME=$(grep "name=" "$FILE" | sed 's/"//g' | cut -d "=" -f 2)
-  export CONTAINER_NAME="$NAME"
-  FILE="$HOME/.local/bin/$NAME"
-else
-  FILE="$HOME/.local/bin/host"
-fi
-if [ -d "$FILE" ]; then
-  PATH="$FILE:$PATH"
+  export CONTAINER_NAME="$(grep "name=" "$FILE" | sed 's/"//g' | cut -d "=" -f 2)"
 fi
 unset FILE
--- .config/shellrc/10.localfs.sh
+++ .config/shellrc/10.localfs.sh
@@ -3,6 +3,16 @@ DIR="$HOME/.local/bin"
 if [ -d "$DIR" ]; then
   export PATH="$DIR:$PATH"
 fi
+
+if [ -n "$CONTAINER_NAME" ]; then
+  DIR="$DIR/$CONTAINER_NAME"
+else
+  DIR="$DIR/host"
+fi
+if [ -d "$DIR" ]; then
+  PATH="$DIR:$PATH"
+fi
+
 DIR="/opt/fs/root"
 if [ -d "$DIR" ]; then
   export PATH="$DIR/bin:$PATH";
