cb0ef8f40693bf2216801b9fc4a15b579f3910d2
Sean Enck <sean@ttypty.com>
2024-10-23 19:27:57 -0400

    only do this if parent exists

--- .config/shellrc/10.localfs.sh
+++ .config/shellrc/10.localfs.sh
@@ -2,15 +2,14 @@
 DIR="$HOME/.local/bin"
 if [ -d "$DIR" ]; then
   export PATH="$DIR:$PATH"
-fi
-
-if [ -n "$CONTAINER_NAME" ]; then
-  DIR="$DIR/$CONTAINER_NAME"
-else
-  DIR="$DIR/host"
-fi
-if [ -d "$DIR" ]; then
-  PATH="$DIR:$PATH"
+  if [ -n "$CONTAINER_NAME" ]; then
+    DIR="$DIR/$CONTAINER_NAME"
+  else
+    DIR="$DIR/host"
+  fi
+  if [ -d "$DIR" ]; then
+    PATH="$DIR:$PATH"
+  fi
 fi
 
 DIR="/opt/fs/root"
