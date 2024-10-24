338fe584bd7ff65c2c9f7c55b5bfad103bd13f70
Sean Enck <sean@ttypty.com>
2024-10-24 13:55:45 -0400

    more configs, some migration to local installs

--- .config/shellrc/30.desktop.sh
+++ .config/shellrc/30.desktop.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-if command -v flatpak >/dev/null; then
-  for APP in $(flatpak list --columns app --app | tail -n +1); do
-    NAME=$(echo "$APP" | rev | cut -d "." -f 1 | rev)
-    {
-      cat << EOF
+DIR="$HOME/.local/share/applications/"
+if [ -d "$DIR" ]; then
+  if command -v flatpak >/dev/null; then
+    for APP in $(flatpak list --columns app --app | tail -n +1); do
+      NAME=$(echo "$APP" | rev | cut -d "." -f 1 | rev)
+      {
+        cat << EOF
 [Desktop Entry]
 Version=1.0
 Name=$NAME
@@ -13,6 +15,7 @@ Exec=flatpak run $APP
 Terminal=false
 Type=Application
 EOF
-    } > "$HOME/.local/share/applications/$APP.desktop"
-  done
+      } > "$HOME/.local/share/applications/$APP.desktop"
+    done
+  fi
 fi
--- .config/shellrc/50.lb.sh
+++ .config/shellrc/50.lb.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-if command -v lb > /dev/null; then
-  export SECRET_ROOT="$HOME/Env/secrets"
+SECRETS="$HOME/Env/secrets"
+if [ -d "$SECRETS" ]; then
+  export SECRET_ROOT="$SECRETS"
   LB_ENV="$SECRET_ROOT/db/lockbox.bash"
   if [ -e "$LB_ENV" ]; then
     source "$LB_ENV"
   fi
   unset LB_ENV 
 fi
+unset SECRETS
