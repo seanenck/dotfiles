db820ff5cd5211b0d134a00f4c4c129197db57b5
Sean Enck <sean@ttypty.com>
2024-10-25 19:51:58 -0400

    rename/categorize files

--- .config/shellrc/20.terminal.system.sh
+++ .config/shellrc/20.terminal.system.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 export TERMINAL_FILE="$HOME/.local/state/terminal"
 if [ ! -e "$TERMINAL_FILE" ]; then
+  FOUND=0
   for TERMINAL in kitty alacritty wezterm foot; do
     if command -v "$TERMINAL" > /dev/null; then
+      FOUND=1
       echo "$TERMINAL" > "$TERMINAL_FILE"
       break
     fi
   done
+  if [ "$FOUND" -eq 0 ]; then
+    echo "unable to set terminal, unknown type/not found?"
+  fi
   unset TERMINAL
 fi
--- .config/shellrc/90.manage-data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if command -v manage-data > /dev/null; then
-  (manage-data tasks &)
-  manage-data errors
-fi
