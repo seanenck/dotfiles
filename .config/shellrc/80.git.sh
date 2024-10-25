12b1f19da520ecf2dee835649648c98eaaba3776
Sean Enck <sean@ttypty.com>
2024-10-25 19:04:28 -0400

    merging dotfile definitions

--- .config/shellrc/80.git.sh
+++ .config/shellrc/80.git.sh
@@ -6,6 +6,7 @@ if command -v git-dotfiles >/dev/null; then
   fi
   if [ "$(uname)" = "Linux" ]; then
     export GIT_DOTFILES_HOST="$(grep '^ID=' /etc/os-release | cut -d "=" -f 2 | sed 's/"//g')"
+    export GIT_DOTFILES_CATEGORY="development"
   fi
 fi
 if command -v git-uncommitted >/dev/null; then
