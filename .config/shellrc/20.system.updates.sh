ed87cd253e1c01345c8aad0dd79d8df51168e13f
Sean Enck <sean@ttypty.com>
2024-11-02 08:46:50 -0400

    check for commands before making function

--- .config/shellrc/20.system.updates.sh
+++ .config/shellrc/20.system.updates.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-sysupdate() {
-  if command -v flatpak >/dev/null; then
-    if ! flatpak update; then
-      echo "flatpak update failed"
-      return
-    fi
+HAS=0
+for ITEM in flatpak blap; do
+  if command -v "$ITEM" >/dev/null; then
+    HAS=1
+    break
   fi
-  if command -v blap >/dev/null; then
-    if ! blap upgrade --commit; then
-      echo "blap updates failed"
-      return
+done
+if [ "$HAS" -eq 1 ]; then
+  sysupdate() {
+    if command -v flatpak >/dev/null; then
+      if ! flatpak update; then
+        echo "flatpak update failed"
+        return
+      fi
     fi
-  fi
-}
+    if command -v blap >/dev/null; then
+      if ! blap upgrade --commit; then
+        echo "blap updates failed"
+        return
+      fi
+    fi
+  }
+fi
+unset HAS ITEM
