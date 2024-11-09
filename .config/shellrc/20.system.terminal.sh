d71ceaeae4fcfb40b3e226d17a127a3a49383d5a
Sean Enck <sean@ttypty.com>
2024-11-09 14:59:47 -0500

    allow non-desktop

--- .config/shellrc/20.system.terminal.sh
+++ .config/shellrc/20.system.terminal.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-FOUND=0
-for TERMINAL in kitty alacritty wezterm foot; do
-  if command -v "$TERMINAL" > /dev/null; then
-    FOUND=1
-    export TERMINAL_EMULATOR="$TERMINAL"
-    break
+if [ -n "$XDG_CURRENT_DESKTOP" ]; then
+  FOUND=0
+  for TERMINAL in kitty alacritty wezterm foot; do
+    if command -v "$TERMINAL" > /dev/null; then
+      FOUND=1
+      export TERMINAL_EMULATOR="$TERMINAL"
+      break
+    fi
+  done
+  if [ "$FOUND" -eq 0 ]; then
+    echo "unable to set terminal, unknown type/not found?"
   fi
-done
-if [ "$FOUND" -eq 0 ]; then
-  echo "unable to set terminal, unknown type/not found?"
 fi
