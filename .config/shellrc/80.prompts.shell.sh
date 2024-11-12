6fa3c8bbbf77645c907c2870dc8b1219fbc4988d
Sean Enck <sean@ttypty.com>
2024-11-11 19:26:21 -0500

    better SHELL support

--- .config/shellrc/80.prompts.shell.sh
+++ .config/shellrc/80.prompts.shell.sh
@@ -5,7 +5,7 @@ if [ -n "$CONTAINER_NAME" ]; then
 fi
 PS1="[\u@\[\e[93m\]$USE_HOST\[\e[0m\]:\W]$ "
 if command -v git-uncommitted >/dev/null; then
-  if [ "$SHELL" = "/bin/bash" ]; then
+  if [ -n "$SHELL" ] && [ "$(basename $SHELL)" = "bash" ]; then
     PS1="\$(git uncommitted pwd 2>/dev/null)$PS1"
   fi
 fi
