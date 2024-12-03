ffcf8344950d3ad7113bb75616c07d999237109a
Sean Enck <sean@ttypty.com>
2024-12-03 16:27:31 -0500

    - switch go tools to blap - trying to use blap for rust things, but it doesn't work.

--- .config/shellrc/40.dev.cargo.sh
+++ .config/shellrc/40.dev.cargo.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-FILE="$HOME/.cargo/env"
+CARGO="$HOME/.cargo/"
+DIR="${CARGO}bin"
+if [ -e "$DIR" ]; then
+  export PATH="$DIR:$PATH"
+fi
+FILE="${CARGO}env"
 if [ -e "$FILE" ]; then
   source "$FILE"
 fi
-unset FILE
+unset DIR FILE
