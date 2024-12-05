154f5884fa8398197ab384a377c51b51d084baaf
Sean Enck <sean@ttypty.com>
2024-12-04 19:12:16 -0500

    use host version to select alpine info

--- .config/shellrc/05.core.type.sh
+++ .config/shellrc/05.core.type.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ "$(uname)" = "Linux" ]; then
   export HOST_OS="$(grep '^ID=' /etc/os-release | cut -d "=" -f 2 | sed 's/"//g')"
+  export HOST_OS_VERSION="$(grep '^VERSION_ID=' /etc/os-release | cut -d "=" -f 2 | cut -d "." -f 1,2)"
   FILE="/run/.containerenv"
   if [ -e "$FILE" ]; then
     export CONTAINER_NAME="$(grep "name=" "$FILE" | sed 's/"//g' | cut -d "=" -f 2)"
