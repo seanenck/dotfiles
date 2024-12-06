8e434bc4dbefe60ea3de4bb4fe0e7580c8d36b85
Sean Enck <sean@ttypty.com>
2024-12-05 21:15:22 -0500

    dlete old nvim files

--- .config/shellrc/60.app.utils.sh
+++ .config/shellrc/60.app.utils.sh
@@ -3,3 +3,6 @@ if command -v delta > /dev/null; then
   export GIT_PAGER=delta
   export DELTA_PAGER="less -R -c -X"
 fi
+if command -v nvim > /dev/null; then
+  find "$HOME/.local/state/nvim" -type f -mtime +1 -delete
+fi
