1457cf5368265915dd647fd55ffdd49f33dc3050
Sean Enck <sean@ttypty.com>
2024-12-05 21:16:05 -0500

    cleanup files

--- .config/shellrc/60.app.utils.sh
+++ .config/shellrc/60.app.utils.sh
@@ -4,5 +4,8 @@ if command -v delta > /dev/null; then
   export DELTA_PAGER="less -R -c -X"
 fi
 if command -v nvim > /dev/null; then
-  find "$HOME/.local/state/nvim" -type f -mtime +1 -delete
+  STATE="$HOME/.local/state/nvim"
+  if [ -d "$STATE" ]; then
+    find "$STATE" -type f -mtime +1 -delete
+  fi
 fi
