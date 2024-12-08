315cf770f56ea3045debdaf1584e2ea4f46969c1
Sean Enck <sean@ttypty.com>
2024-12-08 12:16:21 -0500

    these belong here

--- .config/shellrc/40.dev.go.sh
+++ .config/shellrc/40.dev.go.sh
@@ -3,6 +3,12 @@ for DIR in "Library" ".cache"; do
   DIR="$HOME/$DIR"
   if [ -d "$DIR" ]; then
     export GOPATH="$DIR/go"
+    for MODULE in gopls staticcheck; do
+      SUB="$DIR/$MODULE"
+      if [ -d "$SUB" ]; then
+        find "$SUB" -type f -mtime +1 -delete
+      fi
+    done
   fi
 done
 unset DIR
