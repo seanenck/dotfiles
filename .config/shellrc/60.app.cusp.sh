8e8ccf23cca416a3170f694d1bb4f5b13b547981
Sean Enck <sean@ttypty.com>
2024-12-10 16:27:39 -0500

    migrating where some components are setup

---
+++ .config/shellrc/60.app.cusp.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+if [ "$HOST_OS" = "cusp" ]; then
+  # generate abuild.conf (if needed)
+  ABUILD="$HOME/.abuild"
+  if [ -d "$ABUILD" ]; then
+    CONF="$ABUILD/abuild.conf"
+    if [ ! -e "$CONF" ]; then
+      KEY=$(ls "$ABUILD" | grep '\.rsa$' | grep "dev")
+      ROOT_PACKAGES="$HOME/Linux/packages"
+      {
+        echo "# generated file: $(date +%Y-%m-%dT%H:%M:%S)"
+        cat << EOF
+PACKAGER="Sean Enck <sean@ttypty.com>"
+REPOROOT="$ROOT_PACKAGES"
+REPODEST="$ROOT_PACKAGES/cusp/next"
+EOF
+        if [ -n "$KEY" ]; then
+          echo PACKAGER_PRIVKEY="$ABUILD/$KEY"
+        fi 
+      } > "$CONF"
+    fi
+    unset CONF KEY ROOT_PACKAGES
+  fi
+  unset ABUILD
+
+  # backup apkovl
+  NAME="dev.apkovl.tar.gz"
+  MEDIA="/media/apkovl/$NAME"
+  BACKUP_TO="$HOME/Linux/$NAME"
+  if [ -e "$MEDIA" ]; then
+    if [ -e "$BACKUP_TO" ]; then
+      if [ "$(find "$MEDIA" -type f -newer "$BACKUP_TO" | wc -l)" -eq 0 ]; then
+        return
+      fi
+    fi
+    cp "$MEDIA" "$BACKUP_TO"
+  fi
+fi
