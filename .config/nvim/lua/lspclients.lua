b0fddf1017045845d3a771a6d47ed8eb1976eec4
Sean Enck <sean@ttypty.com>
2024-12-12 17:30:47 -0500

    more cleanup

--- .config/nvim/lua/lspclients.lua
+++ .config/nvim/lua/lspclients.lua
@@ -26,33 +26,5 @@ function get_clients()
           end
         )
     end
-    configurations["efm-langserver"] = function(options)
-        new_client_autocmd({'sh'}, function(args)
-                return {
-                    name = options.name,
-                    capabilities = options.capabilities,
-                    init_options = {documentFormatting = false},
-                    cmd = {options.name},
-                    settings = {
-                        rootMarkers = {".git/"},
-                        languages = {
-                            sh = {
-                                {
-                                    lintCommand = 'shellcheck -f gcc -x',
-                                    lintSource = 'shellcheck',
-                                    lintFormats = {
-                                        '%f:%l:%c: %trror: %m',
-                                        '%f:%l:%c: %tarning: %m',
-                                        '%f:%l:%c: %tote: %m'
-                                    },
-                                    lintIgnoreExitCode = true
-                                }
-                            }
-                        }
-                    }
-                }
-            end
-        )
-    end
     return configurations
 end
