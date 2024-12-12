ee12e99d8de229ff1eae259798e0ffbfa95aef5a
Sean Enck <sean@ttypty.com>
2024-12-12 17:44:27 -0500

    this should be a better setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,14 @@
--- airline settings
 vim.g.airline_extensions = {"tabline"}
 vim.g.airline_extensions["tabline"] = {["formatter"] = "unique_tail_improved"}
 
--- enable LSPs
 require("lsp")
+
+require("scrollbar").setup({})
+
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+  callback = function()
+    if vim.bo.filetype == "sh" then
+        require("lint").try_lint("shellcheck")
+    end
+  end,
+})
