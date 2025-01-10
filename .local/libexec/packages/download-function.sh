e549b5ee68dd13c9d8b48f8ff229c8b85c99951d
Sean Enck <sean@ttypty.com>
2025-01-10 09:29:58 -0500

    cleaning up after packaging

--- .local/libexec/packages/download-function.sh
+++ .local/libexec/packages/download-function.sh
@@ -5,8 +5,10 @@ download() {
     release=$(latest_release "$1" "$2")
   fi
   [ -z "$release" ] && echo "no release found: $1" && exit 1
-  base="${PKGS_DIR}/$4$(basename "$release")"
-  export PKGS_HASH="$(echo "$release" | sha256sum | cut -c 1-7)"
+  prefix="$(echo "$1" | sha256sum | cut -c 1-7)"
+  release_name="$(basename "$release")"
+  base="${PKGS_DIR}/$prefix.$4$release_name"
+  export PKGS_HASH="$prefix"
   [ -e "$base" ] && return
   >&2 echo "downloading release for $1"
   >&2 echo "  -> url: $1"
@@ -16,4 +18,5 @@ download() {
   [ -z "$tmpbase" ] && return
   unpack "$tmpbase"
   mv "$tmpbase" "$base"
+  find "${PKGS_DIR}/" -maxdepth 1 -mtime +7 -type f -name "$prefix*" -not -path "*$release_name*" -delete
 }
