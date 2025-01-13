8ab464a9d351db0490d0df809e0517e067c1025c
Sean Enck <sean@ttypty.com>
2025-01-12 22:03:52 -0500

    check kernel version

--- .config/packages/75.alpine.sh
+++ .config/packages/75.alpine.sh
@@ -16,3 +16,8 @@ tag=$(git_tags "https://gitlab.alpinelinux.org/alpine/aports.git/" | grep "refs/
 major=$(echo "$tag" | cut -d "." -f 1)
 minor=$(echo "$tag" | cut -d "." -f 2)
 download "alpine/aports" "" "https://dl-cdn.alpinelinux.org/alpine/v$major.$minor/releases/$PKGS_ARCH/alpine-standard-$tag-$PKGS_ARCH.iso"
+pkgver=$(curl --silent -L "https://gitlab.alpinelinux.org/alpine/aports/-/raw/$major.$minor-stable/main/linux-lts/APKBUILD?ref_type=heads" | grep -E '^(pkgver|pkgrel)=' | cut -d "=" -f 2- | tr '\n' '-' | sed 's/-$//g')
+[ -z "$pkgver" ] && echo "unable to read alpine pkgver for kernel" && return
+kernel="$(uname -r | rev | cut -d "-" -f 2- | rev)"
+[ "$pkgver" != "$kernel" ] && echo "$pkgver != $kernel" && exit 1
+echo "alpine version checked"
